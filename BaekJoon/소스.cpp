f745540307452e4294cef6fa5a0d61467b9f75dd
PohangCandy <rlcks777@naver.com>
2024-07-08 14:50:21 +0900

    15552

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
 using namespace std;
 
+
+
 int main()
 {
-	long long X,N,a,b;
-	long long sum = 0;
-	
-	cin >> X;
-	cin >> N;
-	for (int i = 0; i < N; i++)
+	ios_base::sync_with_stdio(false); //C 와 C++ 비동기 처리
+	cin.tie(nullptr);// cin과 cout 연결 끊기, 버퍼 나중에 한번에 처리 방식
+
+	int T,A,B;
+	int* P;
+	cin >> T;
+	P = new int[T];
+	for (int i = 0; i < T; i++)
 	{
-		cin >> a >> b;
-		sum += a * b;
+		cin >> A >> B;
+		P[i] = A + B;
 	}
-	if (1 <= X <= 1000000000 && 1 <= N <= 100 && 1 <= a <= 1000000 && 1 <= b <= 10)
+	if (T <= 1000000 && 1 <= A <= 1000 && 1 <= B <= 1000)
 	{
-		if (X == sum)
+		for (int i = 0; i < T; i++)
 		{
-			cout << "Yes";
+			cout << P[i] << "\n"; //endl은 버퍼를 즉시 출력하므로 \n을 써서 버퍼 출력막기
 		}
-		else {
-			cout << "No";
-		}
-
 	}
-
 
 
 	return 0;
