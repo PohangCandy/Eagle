6e29f2885dd699398b1d9333ff3b37695cf8861e
PohangCandy <rlcks777@naver.com>
2024-07-08 12:51:57 +0900

    25304

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -3,46 +3,29 @@ using namespace std;
 
 int main()
 {
-	int A,B,C;
-	int sameEye;
-	int BiggestEye;
-
-	cin >> A;
-	cin >> B;
-	cin >> C;
-
-	if (1 <= A && A<= 6 && 1 <= B && B <= 6 && 1 <= C && C <= 6)
+	long long X,N,a,b;
+	long long sum = 0;
+	
+	cin >> X;
+	cin >> N;
+	for (int i = 0; i < N; i++)
 	{
-		if (A != B && A != C && B != C)
+		cin >> a >> b;
+		sum += a * b;
+	}
+	if (1 <= X <= 1000000000 && 1 <= N <= 100 && 1 <= a <= 1000000 && 1 <= b <= 10)
+	{
+		if (X == sum)
 		{
-			BiggestEye = A;
-			if (B > A && B > C)
-			{
-				BiggestEye = B;
-			}
-			else if(C > A && C > B)
-			{
-				BiggestEye = C;
-			}
-			cout << BiggestEye * 100;
-		}
-		else if ((A == B && B != C) || (A == C && A != B))
-		{
-			sameEye = A;
-			cout << 1000 + sameEye * 100;
-		}
-		else if (A != B && B == C)
-		{
-			sameEye = B;
-			cout << 1000 + sameEye * 100;
+			cout << "Yes";
 		}
 		else {
-			cout << 10000 + A * 1000;
+			cout << "No";
 		}
+
 	}
-	
-	
+
+
 
 	return 0;
 }
-
