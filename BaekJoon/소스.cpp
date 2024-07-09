d2a6b5587c7a13104d7b00fdde6a4d67844979d4
PohangCandy <rlcks777@naver.com>
2024-07-09 12:29:03 +0900

    10951

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -4,22 +4,18 @@ using namespace std;
 
 int main()
 {
-	ios_base::sync_with_stdio(false); //c,c++ 끊어주기
-	cin.tie(NULL); //cin,cout 자동 버버 출혈 해제
+	ios_base::sync_with_stdio(false); //c,c++ 연결 끊어주기
+	cin.tie(NULL); //cin, cout 자동 버퍼 소멸 해제
+
 	int A, B;
-	vector<int> vAB;
+	vector<int>  vAB;
 
-
-	while(1)
+	while (cin >> A >> B)
 	{
-		cin >> A >> B;
-		if (A == 0 && B == 0)
-		{
-			break;
-		}
 		vAB.push_back(A + B);
 	}
-	for (int i = 0; i < vAB.size(); i++)
+
+	for(int i = 0; i < vAB.size(); i++)
 	{
 		cout << vAB[i] << "\n";
 	}
