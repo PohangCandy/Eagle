b6bf29c8b1eaac9b551572ed47f6dd8188ba0f24
PohangCandy <rlcks777@naver.com>
2024-07-15 09:38:16 +0900

    11718

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,48 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
 int main()
 {
-	int A,B,C;
-	int sameEye;
-	int BiggestEye;
+	string s;
+	vector<string> v;
 
-	cin >> A;
-	cin >> B;
-	cin >> C;
-
-	if (1 <= A && A<= 6 && 1 <= B && B <= 6 && 1 <= C && C <= 6)
+	while (getline(cin,s))
 	{
-		if (A != B && A != C && B != C)
-		{
-			BiggestEye = A;
-			if (B > A && B > C)
-			{
-				BiggestEye = B;
-			}
-			else if(C > A && C > B)
-			{
-				BiggestEye = C;
-			}
-			cout << BiggestEye * 100;
-		}
-		else if ((A == B && B != C) || (A == C && A != B))
-		{
-			sameEye = A;
-			cout << 1000 + sameEye * 100;
-		}
-		else if (A != B && B == C)
-		{
-			sameEye = B;
-			cout << 1000 + sameEye * 100;
-		}
-		else {
-			cout << 10000 + A * 1000;
-		}
+		v.push_back(s);
 	}
-	
-	
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << "\n";
+	}
+
 
 	return 0;
 }
-
