c879eace2f216dceb272c6f19cd9b55dc55bf13d
PohangCandy <rlcks777@naver.com>
2024-07-23 16:00:04 +0900

    2869

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main()
@@ -7,39 +6,19 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int T;
-	cin >> T;
-	vector<int> Quarter;
-	vector<int> Dime;
-	vector<int> Nickel;
-	vector<int> Penny;
+	int A, B, V;
+
+	cin >> A >> B >> V;
+
+	if (((V - A) % (A - B)) == 0)
+	{
+		cout << ((V - A) / (A - B)) + 1;
+	}
+	else
+	{
+		cout << ((V - A) / (A - B)) + 2;
+	}
 	
-	for (int i = 0; i < T; i++)
-	{
-		int lastMoney;
-		cin >> lastMoney;
-		
-		int Q = lastMoney / 25;
-		Quarter.push_back(Q);
-		lastMoney -= 25 * Q;
-
-		int D = lastMoney / 10;
-		Dime.push_back(D);
-		lastMoney -= 10 * Q;
-
-		int N = lastMoney / 5;
-		Dime.push_back(N);
-		lastMoney -= 5 * N;
-
-		int P = lastMoney / 1;
-		Dime.push_back(P);
-		lastMoney -= 1 * P;
-	}
-
-	for (int i = 0; i < T; i++)
-	{
-		cout << Quarter[i] << " " << Dime[i] << " " << Nickel[i] << " " << Penny[i];
-	}
 
 	return 0;
 }
