1c7359627ecfa5940d233bdf5c08386acf4f0c67
PohangCandy <rlcks777@naver.com>
2024-07-27 15:54:53 +0900

    finish math

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,24 +1,37 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
 
-	int A, B, V;
+	int N;
+	cin >> N;
 
-	cin >> A >> B >> V;
+	// 소인수분해 
+	// 2부터 나머지가 0이 되는 수를 찾고
+	// 제일 처음에 나눠지는 값으로 나눈 후
+	// 다시 2부터 나눠지는 수를 찾는다.
+	// 해당 작업을 반복한 후,
+	// 몫이 1이되면 해당 작업을 멈춘다.
 
-	if (((V - A) % (A - B)) == 0)
+	while (1)
 	{
-		cout << ((V - A) / (A - B)) + 1;
+		for (int i = 2; i <= N; i++)
+		{
+			if (N % i == 0)
+			{
+				cout << i << "\n";
+				N = N / i;
+				break;
+			}
+		}
+		if (N == 1)
+			break;
 	}
-	else
-	{
-		cout << ((V - A) / (A - B)) + 2;
-	}
-	
+
+
 
 	return 0;
 }
