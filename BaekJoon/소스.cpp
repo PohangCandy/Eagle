3142c0a056d2deace0d263b7116d89c85d1a44d6
PohangCandy <rlcks777@naver.com>
2024-08-06 20:22:33 +0900

    1018

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,34 +1,47 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 
-	//钦己荐客 积己磊
-	int N;
-	int result = 0;
-	cin >> N;
+	int a, b, c, d, e, f;
 
+	cin >> a >> b >> c >> d >> e >> f;
 
-	for (int i = 0; i < N; i++)
+	int x, y;
+	vector<int> first;
+	for (int i = -999; i <= 999; i++)
 	{
-		int sum = 0;
-		int num = i;
-
-		while (num != 0)
+		for (int j = -999; j <= 999; j++)
 		{
-			sum += num % 10;
-			num /= 10;
-		}
-		if (sum + i == N)
-		{
-			result = i;
-			break;
+			if ((a  * i) + (b  * j) == c)
+			{
+				first.push_back(i);
+				first.push_back(j);
+			}
 		}
 	}
 
-	cout << result;
+	for (int i = 0; i < first.size(); i++)
+	{
+		if (i % 2 == 0)
+		{
+			x = first[i];
+			y = first[i + 1];
+			if ((d * x) + (e * y) == f)
+			{
+				break;
+			}
+		}
+	}
+
+	cout << x << " " << y;
+
+	//(a+d)x + (b+e)y = c+f
+	//ax + by = c
+	
 
 	return 0;
 }
