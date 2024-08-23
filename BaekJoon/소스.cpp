0da3f785817f6d74187e7491012d93c660534640
PohangCandy <rlcks777@naver.com>
2024-08-23 15:26:28 +0900

    11650

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
-	vector <int> vec;
+
 	int N;
 	cin >> N;
+
+
+	for (int i = 0; i < N; i++)
+	{
+
+	}
 
 	return 0;
 }
