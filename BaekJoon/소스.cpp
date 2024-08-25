9dfd1d39a0f3736e11692c99eae562ad7560bfa6
PohangCandy <rlcks777@naver.com>
2024-08-25 19:35:14 +0900

    solve 11650

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,18 +1,40 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
+
+struct  position
+{
+	int x, y;
+};
+
+bool compare(position a,position b)
+{
+	if (a.x == b.x)
+	{
+		return a.y < b.y;
+	}
+
+	return a.x < b.x;
+}
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-
+	position arr[100001];
 	int N;
 	cin >> N;
 
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i].x >> arr[i].y;
+	}
+
+	sort(arr, arr+N, compare);
 
 	for (int i = 0; i < N; i++)
 	{
-
+		cout << arr[i].x << " " << arr[i].y << "\n";
 	}
+
 
 	return 0;
 }
