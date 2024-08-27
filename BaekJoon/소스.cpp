41fe938ca5d886f7108e521b647f4d4e1e815ad0
PohangCandy <rlcks777@naver.com>
2024-08-27 16:24:03 +0900

    18870

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -3,57 +3,37 @@
 #include <algorithm>
 using namespace std;
 
-bool compare(string a, string b)
-{
-	if (a.length() == b.length())
-	{
-		for (int i = 0; i < a.length(); i++)
-		{
-			if (a[i] == b[i]) continue;
-			// 사전적으로 더 큰 친구가 뒤로 가야한다.
-			// 아스키 코드의 값이 더 클수록 사전적으로 뒤에 배치하는 수가 된다.
-			// 더 큰 값은 뒤로 보낸다.
-			else return b[i] > a[i];
-		}
-	}
-
-	// 길이가 길면 뒤로 가야한다.
-	// length가 더 큰 값이 뒤로 가야하므로
-	// a.length > b.length 를 하면 a가 더 클때 앞에 위치한다.
-	// b.length > a.length 를하면 b가 더 클때 a가 앞에 위치한다. 
-	return b.length() > a.length();
-}
-
 int main()
 {
+	// 자신보다 낮은 수가 몇 개 있는지 찾아본다.
+	// 그냥 배열 안에서 자기 자신과 다른 수를 비교해가면 count를 늘리면 되는거 아닌가?
+	// 배열 안에서 자신의 순위로 자기 자신을 바꾼다. 
+
 	ios_base::sync_with_stdio(false);
-	vector<string> vec;
+
 	int N;
 	cin >> N;
+	vector<int> vec;
+	vector<int> vec_copy;
+
 	for (int i = 0; i < N; i++)
 	{
-		string input;
+		int input;
 		cin >> input;
 		vec.push_back(input);
+		vec_copy.push_back(input);
 	}
 
-	// 일단 string 배열에 넣어둔다.
-	// sort 함수의 세번째 인자를 통해서 
-	// 길이로 비교하고, 같다면 아스키 코드로 비교한다.
-	// 중복된 단어는 제거한다.
+	// 중복된 값의 경우 더 이상 count 치지 않는다.
+	// 중복된 값을 제외하고 나열했을 때, 
+	// 인덱스 번호가 해당 숫자보다 더 작은 값의 개수를 보여주게 된다.
 
-	sort(vec.begin(), vec.end(), compare);
-
-	vec.erase(unique(vec.begin(), vec.end()),vec.end());
-
+	sort(vec_copy.begin(), vec_copy.end());
+	vec_copy.erase(unique(vec_copy.begin(), vec_copy.end()), vec_copy.end());
+	
 	for (int i = 0; i < vec.size(); i++)
 	{
-		cout << vec[i];
-		if (i != vec.size() - 1)
-		{
-			cout << "\n";
-		}
+		cout << lower_bound(vec_copy.begin(), vec_copy.end(), vec[i]) - vec_copy.begin() << " ";
 	}
-
 	return 0;
 }
