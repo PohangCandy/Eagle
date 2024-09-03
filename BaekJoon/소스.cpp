fc5483314f210188e0c20f3fdef94d4d3703e366
PohangCandy <rlcks777@naver.com>
2024-09-03 17:46:41 +0900

    1160

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,39 +1,62 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <map>
 using namespace std;
+
+// 포켓몬의 이름과 번호를 연결한다
+// key로 이름을 받고 value에 번호를 입력한다.
+// 근데 이러면 value로 key값을 찾을 수 있나?? 
+// pair를 통해 키 값을 두 쌍으로 만들면 어떨까?
+// 수를 통해서든, 단어를 통해서든 value값을 추출해본다.
 
 int main()
 {
-	// 자신보다 낮은 수가 몇 개 있는지 찾아본다.
-	// 그냥 배열 안에서 자기 자신과 다른 수를 비교해가면 count를 늘리면 되는거 아닌가?
-	// 배열 안에서 자신의 순위로 자기 자신을 바꾼다. 
-
 	ios_base::sync_with_stdio(false);
+	map<string, int> dic;
 
-	int N;
-	cin >> N;
-	vector<int> vec;
-	vector<int> vec_copy;
 
+	int N, M;
+	cin >> N >> M;
 	for (int i = 0; i < N; i++)
 	{
-		int input;
+		string input;
 		cin >> input;
-		vec.push_back(input);
-		vec_copy.push_back(input);
+		dic[input] = i + 1;
 	}
 
-	// 중복된 값의 경우 더 이상 count 치지 않는다.
-	// 중복된 값을 제외하고 나열했을 때, 
-	// 인덱스 번호가 해당 숫자보다 더 작은 값의 개수를 보여주게 된다.
-
-	sort(vec_copy.begin(), vec_copy.end());
-	vec_copy.erase(unique(vec_copy.begin(), vec_copy.end()), vec_copy.end());
-	
-	for (int i = 0; i < vec.size(); i++)
+	for (int i = 0; i < M; i++)
 	{
-		cout << lower_bound(vec_copy.begin(), vec_copy.end(), vec[i]) - vec_copy.begin() << " ";
+		string input;
+		cin >> input;
+		//이름이 주어질 경우 번호를 출력한다.
+		if(dic.find(input) != dic.end())
+			cout << dic[input] <<"\n";
+		//번호가 주어질 경우 이름을 어떻게 출력할까?
+		else {
+			int size = input.size();
+			int input_int = 0;
+			for (auto& b : input)
+			{
+				int size_c = size;
+				int i;
+				i = (b - '0');
+				while(size_c != 1)
+				{
+					i *= 10;
+					size_c--;
+				}
+				input_int += i;
+				size--;
+			}
+			for (auto& a : dic)
+			{
+
+			if (a.second == input_int)
+			{
+				cout << a.first << "\n";
+			}
+			}
+		}
 	}
+
 	return 0;
 }
