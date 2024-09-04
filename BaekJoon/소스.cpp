1e30bffcadbdb7512360f19ae622c8b1aab55d7f
PohangCandy <rlcks777@naver.com>
2024-09-04 14:01:21 +0900

    solving 11478

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -11,22 +11,16 @@ int main()
 	string S;
 	cin >> S;
 
-	set<string> st; 
+	set<string> st;
 	string S_part;
-	//문자열의 길이를 계속 늘리기위해
-	//삼중 포문을 써본다.
-	
-	for (int i = 1; i <= S.size(); i++)
+
+	for (int i = 0; i < S.size(); i++)
 	{
 		auto it = S.begin();
-
-		//string 클래스 반복자가 튕긴다고 쥰나 오류나는데 뭐지?
-		while (it != S.end())
+		while (it  + i != S.end())
 		{
 			S_part.clear();
-			//마지막 요소일때 범위 초과가 일어나는 것 같다.
-			//for문에 조건을 추가했는데도 오류가 난다.
-			for (auto j = it; j != it + i && j != S.end(); j++)
+			for (auto j = it; j <= it + i; j++)
 			{
 				S_part += *j;
 			}
@@ -35,10 +29,7 @@ int main()
 		}
 	}
 
-	for (auto& a : st)
-	{
-		cout << a << " ";
-	}
+	cout << st.size();
 
 	return 0;
 }
