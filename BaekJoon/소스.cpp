d13b134a9d44c05c3416541f6d1d7e800bed9ba4
PohangCandy <rlcks777@naver.com>
2024-09-06 20:47:18 +0900

    2485

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,55 +1,55 @@
-// 두 수를 입력 받아서 최소공배수를 출력하자.
-// 하나의 수의 배수를 구한 후, 다른 수로 나눠서 나머지가 0이면 공배수가 된다.
-// 더 큰 수를 찾아서 해당 수의 배수를 작은 수로 나누는 작업이 더 효율적이다.
+// 균일하게 가로수를 심기
+// 주어진 거리 중에 가장 짧은 간격을 구한다.
+// 3,2가 각 거리인 경우 간격의 거리가 2가 되면 안된다.
+// 주어진 거리들의 최대공약수를 찾아야한다.
+// 가장 작은 가로수 위치에서
+// 최대공약수만큼 더한 값이 컨테이너에 없다면 추가하고 있다면 넘어간다.
+// 해당 작업은 컨테이너 내의 가장 큰 값을 넘지 않을 때까지 반복한다.
 
 #include <iostream>
+//가로수를 추가하기위해 벡터를 쓰고
+#include <vector>
+//정렬하기위해 sort를 쓴다.
+#include <algorithm>
 using namespace std;
+
+//최대공약수를 찾기 위한 함수
+int GCD(int bigger, int smaller)
+{
+	if (bigger % smaller == 0)
+	{
+		return smaller;
+	}
+	else
+	{
+		return GCD(smaller, bigger % smaller);
+	}
+}
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	vector<int> vec;
+	vector<int> distance;
 
-	int T;
-	cin >> T;
-
-	for (int i = 0; i < T; i++)
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++)
 	{
-		int first, second;
-		int common_mutiple;
-
-		cin >> first >> second;
-		if (first >= second)
-		{
-			int i = 1;
-			while (1)
-			{
-				int first_copy = first * i;
-				if (first_copy % second == 0)
-				{
-					common_mutiple = first_copy;
-					break;
-				}
-				i++;
-			}
-			cout << common_mutiple << "\n";
-		}
-		else
-		{
-			int i = 1;
-			while (1)
-			{
-				int second_copy = second * i;
-				if (second_copy % first == 0)
-				{
-					common_mutiple = second_copy;
-					break;
-				}
-				i++;
-			}
-			cout << common_mutiple << "\n";
-		}
-
+		int input;
+		cin >> input;
+		vec.push_back(input);
 	}
+
+	sort(vec.begin(), vec.end());
+
+	auto it = vec.begin();
+	while (it != vec.end())
+	{
+		distance.push_back(*(it + 1) - (*it));
+	}
+
 
 	return 0;
 }
