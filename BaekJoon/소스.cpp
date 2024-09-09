5ed141496bf050857b06bbe0520f7e655f01232a
PohangCandy <rlcks777@naver.com>
2024-09-09 19:34:18 +0900

    [200~

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,63 +1,49 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
-
 using namespace std;
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	vector<long long> vec;
+	
+	int N, M;
+	cin >> N >> M;
 
-	long long test;
-	cin >> test;
+	vector<int> vec(M + 1, 0);
 
-	for (long long i = 0; i < test; i++)
+	//0,1은 소수가 아니므로 0으로 놔둔다.
+	//크기가 M인 벡터에 2부터 인덱스와 일치한 수를 넣어둔 후, 
+	for (int i = 2; i <= M; i++)
 	{
-		long long input;
-		cin >> input;
-		long long copy = input;
-		//0,1을 넣으면 2가 출력된다.
-		if (copy < 2)
-		{
-			copy = 2;
-			vec.push_back(copy);
-		}
-		else {
-			while (1)
-			{
-				//벡터의 사이즈로 소수가 추가된 걸 확인
-				long long size = vec.size();
-				for (long long j = 2; j <= sqrt(copy); j++)
-				{
-					if (copy % j == 0)
-					{
-						break;
-					}
-					else if (j  + 1 > sqrt(copy))
-					{
-						vec.push_back(copy);
-						break;
-					}
-				}
-				if (size != vec.size())
-				{
-					break;
-				}
-				copy++;
-			}
-		}
-
+		vec[i] = i;
 	}
 
-	for (auto& a : vec)
+	//벡터 내부에서 이중 포문을 사용해서
+	for (int i = 2; i * i <= M; i++)
 	{
-		cout << a << "\n";
+		//만약 자기자신의 값이 0이라면
+		//이미 다른수의 배수라는 뜻이므로 건너뛴다.
+		if (vec[i] == 0) continue;
+		//아니라면 자기 자신을 제외한 배수들에 0을 대입한다.
+		else
+		{
+			for (int j = i * 2; j <= M; j += i)
+			{
+				//공배수의 경우 이미 0이 되어 있을 수 있다.
+				if (vec[j] == 0) continue;
+				else vec[j] = 0;
+			}
+		}
+	}
+
+	//주어진 입력 범위에서 0이 아닌 값만 출력하면 된다.
+	for (int i = N; i <= M; i++)
+	{
+		if (vec[i] != 0)
+		{
+			cout << vec[i] << " ";
+		}
 	}
 
 	return 0;
 }
-
-
-
