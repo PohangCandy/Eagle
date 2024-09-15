945e1ac9105e53afb24fc938ebfd0b3e1e867628
PohangCandy <rlcks777@naver.com>
2024-09-15 17:40:53 +0900

    2346

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,5 +1,8 @@
+//원형으로 배치된 풍선을 쪽지 순서대로 터트리기
+
 #include <iostream>
-#include <queue>
+#include <deque>
+#include <vector>
 using namespace std;
 
 int main()
@@ -7,42 +10,79 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-	queue<int> que;
-	queue<int> result;
+	deque<int> deq;
+	vector<int> paper;
+	vector<int> result;
 
-	int N, K;
-	cin >> N >> K;
-	// 입력 - 1 만큼의 수를 다시 큐에 삽입하고
-	// 입력한 인덱스의 요소 제거
-	// 모든 요소가 제거 될 때까지 반복
-	for (int i = 1; i <= N; i++)
+	int N;
+	cin >> N;
+	// 덱을 이용해서 남아있는 풍선의 번호를 표현한다.
+	// 2 ~ N까지 저장되어있는 덱을 만든다.
+	for (int i = 2; i <= N; i++)
 	{
-		que.push(i);
+		deq.push_back(i);
+	}
+	//항상 첫 번째 풍선이 먼저 터진다.
+	result.push_back(1);
+
+	for (int i = 0; i < N; i++)
+	{
+		int input;
+		cin >> input;
+		paper.push_back(input);
 	}
 
-	while (!que.empty())
+	// 입력을 받는 횟수
+	// 풍선을 터트리는 횟수는 총 n번이다.
+	// 입력 순서대로 들어가지 않는다.
+	// 입력한 값은 각 풍선에 적혀있는 다음 풍선 지정 번호이다.
+	int index = 1;
+	while (1)
 	{
-		for (int i = 0; i < K - 1; i++)
+		if (deq.empty()) break;
+		//vec는 0부터 시작하므로 -1해준다.
+		int next = paper[index-1];
+
+		if (next > 0)
 		{
-			que.push(que.front());
-			que.pop();
+			// input = 3
+			// 2 3 4 5
+			// 5 2 3 "4"
+			while (next != 0)
+			{
+				deq.push_back(deq.front());
+				deq.pop_front();
+				//cout << "move front_deq " << deq.back() << "to back" << "\n";
+				next--;
+			}
+			result.push_back(deq.back());
+			//cout << "push back_deq " << deq.back() << "to vec" << "\n";
+			index = deq.back();
+			deq.pop_back();
 		}
-		result.push(que.front());
-		que.pop();
-	}
-	cout << "<";
-
-	while (!result.empty())
-	{
-		cout << result.front();
-		result.pop();
-		if (result.size() != 0)
+		else if (next < 0)
 		{
-			cout << ", ";
+			// input = -3
+			// 2 3 4 5
+			// "3" 4 5 2
+			while (next != 0)
+			{
+				deq.push_front(deq.back());
+				deq.pop_back();
+				//cout << "move back_deq " << deq.front() << "to front" << "\n";
+				next++;
+			}
+			result.push_back(deq.front());
+			//cout << "push front_deq " << deq.front() << "to vec" << "\n";
+			index = deq.front();
+			deq.pop_front();
 		}
 	}
 
-	cout << ">";
+	for (auto& a : result)
+	{
+		cout << a << " ";
+	}
 
 	return 0;
 }
