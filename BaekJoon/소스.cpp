ac29f870d72cc7a958219aebfd57a418f93826a7
PohangCandy <rlcks777@naver.com>
2024-09-18 19:07:56 +0900

    24511

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,7 +1,7 @@
-//queuestack
+//deque를 이용해서 한번에 해결할 수도 있다.
 #include <iostream>
-#include <queue>
 #include <vector>
+#include <deque>
 using namespace std;
 
 int main()
@@ -9,23 +9,29 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-	vector<int> questack;
-	vector<int> result;
+	vector<int> questk;
+	deque<int> deq;
 
 	int N;
 	cin >> N;
+	//스택인지 큐인지 판별하는 데이터 저장
 	for (int i = 0; i < N; i++)
 	{
 		int input;
 		cin >> input;
-		questack.push_back(input);
+		questk.push_back(input);
 	}
 
 	for (int i = 0; i < N; i++)
 	{
 		int input;
 		cin >> input;
-		result.push_back(input);
+		//큐의 원소만 덱에 삽입
+		//가장 나중에 들어온 요소를 back으로 pop하기 위해 뒤로 삽입
+		if (questk[i] == 0)
+		{
+			deq.push_back(input);
+		}
 	}
 
 	int M;
@@ -34,22 +40,15 @@ int main()
 	{
 		int input;
 		cin >> input;
-		for (int j = 0; j < questack.size(); j++)
+		//입력한 값이 큐의 원소보다 늦게 나와야하므로 앞으로 삽입
+		deq.push_front(input);
+		cout << deq.back();
+		if (!deq.empty()) 
 		{
-			if (questack[j] == 0)
-			{
-				int temp = result[j];
-				result[j] = input;
-				input = temp;
-			}
-			else
-				continue;
+			deq.pop_back();
+			cout << " ";
 		}
-		cout << input << " ";
 	}
-	// 스택에서 팝하면 최신게나감
-	// 큐에서 팝하면 오래된게 나감
-	
 
 	return 0;
 }
