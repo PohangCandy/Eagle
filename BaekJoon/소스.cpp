63de2601154f9f7c01a01e77cf1597b0ed6067dc
PohangCandy <rlcks777@naver.com>
2024-09-19 20:44:40 +0900

    9093

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,4 +1,13 @@
+// 입력받은 string 객체가 
+// 공백문자를 제외하고 
+// 거꾸러 만들어져야 한다.
+// getline함수로 문장을 입력받은 후에, 
+// 해당 객체를 공백 문자가 나오기 전까지 stack에 담고
+// 공백 문자가 나오면 stack에 있는 값을 
+// 모두 꺼내서 다른 스택에 저장한다.
+
 #include <iostream>
+#include <string>
 #include <stack>
 using namespace std;
 
@@ -7,51 +16,44 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-	stack<int> stk;
+	stack<char> stk;
 
-	int N;
-	cin >> N;
-	
-	for (int i = 0; i < N; i++)
+	int T;
+	cin >> T;
+	cin.ignore();
+
+	for (int i = 0; i < T; i++)
 	{
 		string input;
-		cin >> input;
-		if (input == "push")
+		getline(cin, input);
+		for(auto &a: input)
 		{
-			int x;
-			cin >> x;
-			stk.push(x);
-		}
-		else if (input == "pop")
-		{
-			if (!stk.empty())
+			// 공백을 기준으로 할 경우 
+			// 마지막 단어를 출력시키지 못한다.
+			if (a == ' ')
 			{
-				cout << stk.top() << "\n";
-				stk.pop();
+				while (!stk.empty())
+				{
+					cout << stk.top();
+					stk.pop();
+				}
+				cout << " ";
 			}
-			else cout << -1 << "\n";
-		}
-		else if (input == "size")
-		{
-			cout << stk.size() << "\n";
-		}
-		else if (input == "empty")
-		{
-			if (stk.empty())
+			else
 			{
-				cout << 1 << "\n";
+				stk.push(a);
 			}
-			else cout << 0 << "\n";
 		}
-		else if (input == "top")
+
+		while (!stk.empty())
 		{
-			if (stk.empty())
-			{
-				cout << -1 << "\n";
-			}
-			else cout << stk.top() << "\n";
+			cout << stk.top();
+			stk.pop();
 		}
+		cout << "\n";
 	}
 
+
+		
 	return 0;
 }
