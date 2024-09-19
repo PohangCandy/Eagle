5951a2bc8f41746ee7f3e9d6623555ad26931a1f
PohangCandy <rlcks777@naver.com>
2024-09-19 15:57:25 +0900

    10828

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,7 +1,5 @@
-//deque를 이용해서 한번에 해결할 수도 있다.
 #include <iostream>
-#include <vector>
-#include <deque>
+#include <stack>
 using namespace std;
 
 int main()
@@ -9,44 +7,49 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-	vector<int> questk;
-	deque<int> deq;
+	stack<int> stk;
 
 	int N;
 	cin >> N;
-	//스택인지 큐인지 판별하는 데이터 저장
+	
 	for (int i = 0; i < N; i++)
 	{
-		int input;
+		string input;
 		cin >> input;
-		questk.push_back(input);
-	}
-
-	for (int i = 0; i < N; i++)
-	{
-		int input;
-		cin >> input;
-		//큐의 원소만 덱에 삽입
-		//가장 나중에 들어온 요소를 back으로 pop하기 위해 뒤로 삽입
-		if (questk[i] == 0)
+		if (input == "push")
 		{
-			deq.push_back(input);
+			int x;
+			cin >> x;
+			stk.push(x);
 		}
-	}
-
-	int M;
-	cin >> M;
-	for (int i = 0; i < M; i++)
-	{
-		int input;
-		cin >> input;
-		//입력한 값이 큐의 원소보다 늦게 나와야하므로 앞으로 삽입
-		deq.push_front(input);
-		cout << deq.back();
-		if (!deq.empty()) 
+		else if (input == "pop")
 		{
-			deq.pop_back();
-			cout << " ";
+			if (!stk.empty())
+			{
+				cout << stk.top() << "\n";
+				stk.pop();
+			}
+			else cout << -1 << "\n";
+		}
+		else if (input == "size")
+		{
+			cout << stk.size() << "\n";
+		}
+		else if (input == "empty")
+		{
+			if (stk.empty())
+			{
+				cout << 1 << "\n";
+			}
+			else cout << 0 << "\n";
+		}
+		else if (input == "top")
+		{
+			if (stk.empty())
+			{
+				cout << -1 << "\n";
+			}
+			else cout << stk.top() << "\n";
 		}
 	}
 
