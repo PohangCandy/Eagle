b011f7789af8c83bace2b35ad96810ec698fa0a6
PohangCandy <rlcks777@naver.com>
2024-09-22 18:53:10 +0900

    1406

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,91 +1,74 @@
-// 입력된 수와 스택이 top이 다르다면 
-// push하며 +를 출력하다 
-// 수열에 해당하는 수와 스택의 top이 일치하면
-// pop하고 -를 출력한다.
-// push는 항상 오름차순으로 받으므로,
-// 입력한 값이 스택의 top보다 작다면
-// pop할 수 없으므로
-// No를 출력한다.
-
-
 #include <iostream>
 #include <stack>
-#include <vector>
 using namespace std;
+
+// 스택을 사용해서 문제를 해결해본다.
+// 스택 하나만으론 중간에 있는 요소를 삭제 할 수 없으므로
+// 두개를 만들어 커서를 중심으로 나눠준다.
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-	int n;
-	cin >> n;
-	stack<int> stk;
-	int index = 1;
-	vector<string> result;
-	
-	for (int i = 0; i < n; i++)
+	stack<char> left;
+	stack<char> right;
+
+	string input;
+	cin >> input;
+	for (auto& a : input)
 	{
-		int input;
-		cin >> input;
-		if (stk.empty())
+		left.push(a);
+	}
+
+	int M;
+	cin >> M;
+	for (int i = 0; i < M; i++)
+	{
+		char c;
+		cin >> c;
+		if (c == 'L')
 		{
-			while (index <= n)
+			if (!left.empty())
 			{
-				stk.push(index);
-				result.push_back("+");
-				if (stk.top() == input)
-				{
-					stk.pop();
-					result.push_back("-");
-					index++;
-					break;
-				}
-				index++;
+				right.push(left.top());
+				left.pop();
 			}
 		}
-		else
+		else if (c == 'D')
 		{
-			if (stk.top() == input)
+			if (!right.empty())
 			{
-				stk.pop();
-				result.push_back("-");
+				left.push(right.top());
+				right.pop();
 			}
-			else if(stk.top() < input)
+		}
+		else if (c == 'B')
+		{
+			if (!left.empty())
 			{
-				while (index <= n)
-				{
-					stk.push(index);
-					result.push_back("+");
-					if (stk.top() == input)
-					{
-						stk.pop();
-						result.push_back("-");
-						index++;
-						break;
-					}
-					index++;
-				}
+				left.pop();
 			}
-			else
-			{
-				result.clear();
-				stk.push(0);
-			}
+		}
+		else if (c == 'P')
+		{
+			char p;
+			cin >> p;
+			left.push(p);
 		}
 	}
 
+	while (!left.empty())
+	{
+		right.push(left.top());
+		left.pop();
+	}
 
-	if (stk.empty())
+	while (!right.empty())
 	{
-		for (auto& a : result)
-		{
-			cout << a << "\n";
-		}
+		cout << right.top();
+		right.pop();
 	}
-	else
-	{
-		cout << "NO";
-	}
-	
+
 	return 0;
 }
