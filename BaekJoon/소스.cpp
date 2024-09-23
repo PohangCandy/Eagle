b5d62d521bda960207165a59fcd7bd5d40c3826b
PohangCandy <rlcks777@naver.com>
2024-09-23 15:51:17 +0900

    10866

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,41 +1,154 @@
-// 큐 하나만을로도 해결할 수 있는 문제였다.
 #include <iostream>
-#include <queue>
 using namespace std;
+
+#define MAX_SIZE (10000)
+
+template<typename T>
+class deque {
+private:
+	T data[MAX_SIZE];
+	int _front;
+	int _back;
+public:
+	deque(){
+		_front = 0;
+		_back = 0;
+	}
+	~deque() {
+	}
+
+	void push_front(int inputdata)
+	{
+		for (int i = _back; i > _front; i--)
+		{
+			data[i] = data[i - 1];
+		}
+		data[_front] = inputdata;
+		_back++;
+	}
+
+	void push_back(int inputdata)
+	{
+		data[_back] = inputdata;
+		_back++;
+	}
+
+	void pop_front()
+	{
+		for (int i = 0; i < _back; i++)
+		{
+			data[i] = data[i + 1];
+		}
+		_back--;
+	}
+
+	void pop_back()
+	{
+		_back--;
+	}
+
+	int size()
+	{
+		return _back - _front;
+	}
+
+	bool empty()
+	{
+		return (_back == _front);
+	}
+
+	T front()
+	{
+		T res = data[_front];
+		return res;
+	}
+
+	T back()
+	{
+		T res = data[_back - 1];
+		return res;
+	}
+
+};
+
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-	queue<int> que;
-
-	int N, K;
-	cin >> N >> K;
-
-	// 큐의 가장 앞에 있는 숫자를 다시 큐에 push한다
-	// K번째가 될때마다 가장 앞의 값을 pop하고 출력한다.
-	for (int i = 1; i <= N; i++)
+	deque<int> deq;
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++)
 	{
-		que.push(i);
-	}
-
-	cout << "<";
-	//que의 사이즈가 1남으면 멈추고 출력해준다.
-	while (que.size() > 1)
-	{
-		for (int i = 1; i < K; i++)
+		string input;
+		cin >> input;
+		int X;
+		if (input == "push_front")
 		{
-			que.push(que.front());
-			que.pop();
+			cin >> X;
+			deq.push_front(X);
 		}
-		cout << que.front() <<", ";
-		que.pop();
-	}
-
-	if (!que.empty())
-	{
-		cout << que.front() << ">";
+		else if (input == "push_back")
+		{
+			cin >> X;
+			deq.push_back(X);
+		}
+		else if (input == "pop_front")
+		{
+			if (deq.empty())
+			{
+				cout << -1 << "\n";
+			}
+			else
+			{
+				cout << deq.front() << "\n";
+				deq.pop_front();
+			}
+		}
+		else if (input == "pop_back")
+		{
+			if (deq.empty())
+			{
+				cout << -1 << "\n";
+			}
+			else
+			{
+				cout << deq.back() << "\n";
+				deq.pop_back();
+			}
+		}
+		else if (input == "size")
+		{
+			cout << deq.size() << "\n";
+		}
+		else if (input == "empty")
+		{
+			cout << deq.empty() << "\n";
+		}
+		else if (input == "front")
+		{
+			if (deq.empty())
+			{
+				cout << -1 << "\n";
+			}
+			else
+			{
+				cout << deq.front() << "\n";
+			}
+		}
+		else if (input == "back")
+		{
+			if (deq.empty())
+			{
+				cout << -1 << "\n";
+			}
+			else
+			{
+				cout << deq.back() << "\n";
+			}
+		}
 	}
 
 	return 0;
