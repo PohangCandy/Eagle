4212d43c5546053ef425985de3e65b12dcdc918e
PohangCandy <rlcks777@naver.com>
2024-09-23 14:46:25 +0900

    1158

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,12 +1,6 @@
-// 요세푸스 순열
-// 원모양으로 둘러 앉은 사람들을 번호에 따라 한명씩 빼는 문제
-// 1 - 7 까지 삭제
-// 3 6 2(2) 7(5) 5(3) 1(1) 4
-// 중간에서 요소를 빼므로 리스트를 써서 작성하고
-// 만약 반복자가 리스트의 끝에 위치하면 다시 begin으로 보낸다.
-
+// 큐 하나만을로도 해결할 수 있는 문제였다.
 #include <iostream>
-#include <list>
+#include <queue>
 using namespace std;
 
 int main()
@@ -14,34 +8,34 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-	list<int> lst;
+	queue<int> que;
 
 	int N, K;
 	cin >> N >> K;
+
+	// 큐의 가장 앞에 있는 숫자를 다시 큐에 push한다
+	// K번째가 될때마다 가장 앞의 값을 pop하고 출력한다.
 	for (int i = 1; i <= N; i++)
 	{
-		lst.push_back(i);
+		que.push(i);
 	}
 
-	auto it = lst.begin();
-	int last = 0;
-	int q;
-	while (!lst.empty())
+	cout << "<";
+	//que의 사이즈가 1남으면 멈추고 출력해준다.
+	while (que.size() > 1)
 	{
-		for (int i = 0; i < K - 1; i++)
+		for (int i = 1; i < K; i++)
 		{
-			if (it == lst.end())
-			{
-				it = lst.begin();
-				it++;
-			}
-			else
-			{
-				it++;
-			}
+			que.push(que.front());
+			que.pop();
 		}
-		cout << *it << " ";
-		it = lst.erase(it);
+		cout << que.front() <<", ";
+		que.pop();
+	}
+
+	if (!que.empty())
+	{
+		cout << que.front() << ">";
 	}
 
 	return 0;
