f0b6e1ac10ad4732dbb89149e7742ab45d099754
PohangCandy <rlcks777@naver.com>
2024-09-24 20:21:19 +0900

    17299

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -5,8 +5,9 @@
 #include <stack>
 using namespace std;
 
-//개수 저장
 int arr[1000001];
+//개수 저장
+int num[1000001] {0};
 //자신보다 많은 수 저장
 int res[1000001];
 
@@ -17,7 +18,53 @@ int main()
 
 	int N;
 	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		int input;
+		cin >> input;
+		arr[i] = input;
+		//인덱스 번호에 개수를 저장
+		num[input] += 1;
+	}
 
+	stack<int> stk;
+
+	for (int i = 0; i < N; i++)
+	{
+		// 인덱스 번호를 스택에 push하면서 
+		// num[stk.top()]와 num[arr[i]]를 비교해서
+		// 스택의 탑에 있는 값의 개수가 더 작다면
+		// res[stk.top()] = arr[i]이 된다.
+		//스택이 빌때까지 해당 작업을 반복한다.
+		while (!stk.empty())
+		{
+			// 스택의 인덱스 번호는 0부터 시작하므로
+			// num의 인덱스와 맞추기 위해 +1을 해준다.
+			if (num[arr[i]] > num[arr[stk.top()]])
+			{
+				res[stk.top()] = arr[i];
+				stk.pop();
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		stk.push(i);
+	}
+
+	//스택에 남아있는 친구들에겐 -1을 준다.
+	while (!stk.empty())
+	{
+		res[stk.top()] = -1;
+		stk.pop();
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << res[i] << " ";
+	}
 
 	return 0;
 }
