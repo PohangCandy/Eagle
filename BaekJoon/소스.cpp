d9341c2cf89638309db73bc2ca7d366546820479
PohangCandy <rlcks777@naver.com>
2024-09-24 18:00:50 +0900

    17298

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,68 +1,66 @@
-// 쇠막대기 레이저로 자르기
-// ()가 레이저 ( 가 막대의 시작점이다.
-// )가 나오기 전까지 얼마나 많은 ()쌍이 있는지 찾으면 된다.
-// 막대의 개수는 레이저의 개수 + 1이 된다.
-
-// 레이저가 나오면 현재 있는 막대기들은 모두 짤린다.
-// )가 나오면 가장 최근에 있는( 값을 pop한다
-// pop한 후 남아있는 사이즈가 남아있는 막대의 개수이므로
-// size만큼을 더해준다.
-// 이전에 들어왔던 값이 (가 아니라 
-// )라는 것을 판별해줄 bool값을 넣어서
-// true라면 그 다음에 오는 )는 막대의 끝 부분이다.
-// 이 경우 size를 더하지 않고 pop만 실행해준다.
+// 시간제한이 있으므로 이중 포문으로 구할 수 없다.
+// 입력 받은 값이 이전에 입력받은 값보다 더 큰 경우 해당 값이 오큰수가 된다.
+// 오큰수를 찾지 못한 수의 인덱스 번호를 스택에 저장해두고
+// 다음 입력값과 비교해서 오큰수를 찾았다면
+// pop으로 스택에 있는 인덱스 값을 빼준다.
+// 스택이 빌때까지 값을 비교하다
+// 입력 값이 스택에 있는 인덱스 값보다 작다면 break하고
+// stack에 push 한다.
+// 모든 입력이 끝난 후에도 스택에 남아있는 값들은
+// 오큰수를 -1로 배정해준다.
 
 #include <iostream>
 #include <stack>
 using namespace std;
+
+int arr[1000001];
+int res[1000001];
+stack<int> stk;
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-	string input;
-	cin >> input;
-	stack<char> stk;
-
-	int num = 0;
-	bool laser = false;
-
-	for (auto a : input)
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++)
 	{
-		//막대가 추가된 경우 개수를 증가시켜준다.
-		if (a == '(')
+		int input;
+		cin >> input;
+		arr[i] = input;
+		// 스택이 빌 때까지,
+		// 지금 입력받은 값과 
+		// 스택에 있는 값을 비교한다.
+		while (!stk.empty())
 		{
-			stk.push(a);
-			num++;
-			//다음에 )가 오면 laser가 된다.
-			laser = true;
-		}
-		else if (a == ')')
-		{
-			// 레이저인 경우와 막대의 끝인 경우 두가지가 있다.
-			// 바로 직전에 ( 가 있었다면 레이저가 된다.
-			if (laser)
+			//만약 입력값이 스택의 top이 가르키는 인덱스 값보다 크다면
+			//입력값은 해당 수의 오큰 수가 된다.
+			if (input > arr[stk.top()])
 			{
-				//먼저 직전에 막대로 인식했던 (를 빼준다.
-				num--;
+				res[stk.top()] = input;
 				stk.pop();
-				//현재 막대기 개수만큼 더한다.
-				num += stk.size();
-				//곧 이어 나오는 )는 막대 끝을 의미한다.
-				laser = false;
 			}
+			//작다면 입력값의 인덱스를 스택에 push한다.
 			else
 			{
-				//막대 끝이라면 막대 하나를 스택에서 빼준다.
-				stk.pop();
-				//이후에 )가 와도 역시 막대끝이다.
-				//laser = false;로 유지한다.
+				break;
 			}
 		}
+		stk.push(i);
 	}
 
-	cout << num;
+	//모든 입력을 마친 후에 스택에 남아있는 값들은 -1을 대입
+	while (!stk.empty())
+	{
+		res[stk.top()] = -1;
+		stk.pop();
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << res[i] << " ";
+	}
 
 	return 0;
 }
