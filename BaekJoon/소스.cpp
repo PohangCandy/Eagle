2b16485a0637a048052e31cd00a8fd00fadd93e9
PohangCandy <rlcks777@naver.com>
2024-09-26 16:03:28 +0900

    s

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,3 +1,8 @@
+//stack에 2개의 숫자가 쌓이면
+//top을 그냥 더하고
+//pop한 후에
+//그 다음 top을 *100을 해서 더한다.
+#include <stack>
 #include <iostream>
 using namespace std;
 
@@ -5,8 +10,25 @@ int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
+	stack<int> stk;
 
+	int result = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		int input;
+		cin >> input;
+		stk.push(input);
+		if (stk.size() >= 2)
+		{
+			result += stk.top();
+			stk.pop();
+			//
+			result += stk.top() * 100;
+			stk.pop();
+		}
+	}
 
+	cout << result;
 
 	return 0;
 }
