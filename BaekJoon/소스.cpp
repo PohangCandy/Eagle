71732b43401e7ec1a0ad55ab9c035f87e88d8bcd
PohangCandy <rlcks777@naver.com>
2024-09-28 20:38:25 +0900

    1676

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,81 +1,46 @@
-//에라토스테네스의 체가 여기서도 활용된다.
-//소수 관련 문제면 다 활용되는 듯
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-void IsPrimeNum(bool arr[], int size)
-{
-	//0과 1은 소수가 아니다.
-	arr[0] = false;
-	arr[1] = false;
-	/*25가 소수인지 판단하기 위해서 5까지만 조사하면 되듯이
-	25보다 작은 수는 제곱수라고 해도 약수로 5보다 작은 값을 가질 것이다.
-	그러므로 25보다 적은 수 중 5보다 작은 수의 배수가 아니라면 소수가 된다.*/
-	
-	// 그러므로 모든 size보다 작은 소수를 찾는 작업은
-	// size의 제곱근까지만 조사하면 된다.
-	// 배수들을 false로 만들어준다.
-	for (int i = 0; i * i < size; i++)
-	{
-		//이미 다른 수의 배수라면 넘어간다.
-		if (arr[i] == false) continue;
-		else
-		{
-			// i의 배수들을 찾아준다.
-			// i * 2 , i * 3은 이미 2,3의 배수로 처리되었기 때문에
-			//i * i부터 찾아주자.
-			for (int j = i * i; j < size; j += i)
-			{
-				arr[j] = false;
-			}
-		}
-	}
-}
+//간단한 문제 같은데 퍽킹 시간초과가 발생한다.
+//팩토리얼 공식이 따로 있는건가?
+//N! = 1 * 2 * 3 * ... * N
+//2와 5의 배수 쌍이 얼마나 있는지 찾아볼까?
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-	bool *che = new bool[1000001];
-	//메모리 안의 모든 수를 소수로 초기화
-	fill(che, che + 1000001, true);
-	IsPrimeNum(che, 1000001);
-	bool find;
-	int MaxTextSize = 1000000;
-	while (MaxTextSize > 0)
+	long long N;
+	cin >> N;
+	long long pac = 1;
+	long long count_two = 0;
+	long long count_five = 0;
+	for (long long i = 1; i <= N; i++)
 	{
-		int n;
-		cin >> n;
-		if (n == 0) break;
-
-		find = false;
-		//인덱스 값이 소수일때 true인 배열에서
-		//합이 n인 짝을 찾는다.
-		//인덱스 번호는 n/2까지만 찾으면된다.
-		//홀수인 소수 조합을 찾으므로
-		//홀수인 인덱스 번호만 조사한다.
-
-		for (int i = 3; i <= n / 2; i += 2)
+		int j = i;
+		while (j % 2 == 0)
 		{
-			// 두 수가 소수이면 break하고 출력한다.
-			if (che[i] && che[n - i])
-			{
-				find = true;
-				cout << n << " = " << i << " + " << n - i << "\n";
-				break;
-			}
+			count_two++;
+			j /= 2;
 		}
 
-		if (!find)
+		while(j % 5 == 0)
 		{
-			cout << "Goldbach's conjecture is wrong." << "\n";
+			count_five++;
+			j /= 5;
 		}
-		
-		MaxTextSize--;
 	}
 
-	delete[] che;
+	//둘 중에 더 적은 값이 정답이 된다.
+	if (count_two >= count_five)
+	{
+		cout << count_five;
+	}
+	else
+	{
+		cout << count_two;
+	}
+
 	return 0;
 }
