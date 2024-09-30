b55076d63b9d5b5c50b663e56f39dcec9973d2ff
PohangCandy <rlcks777@naver.com>
2024-09-30 17:20:12 +0900

    1373

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,88 +1,32 @@
+// 주어진 수의 길이가 1,000,000이란 의미는
+// 2^1,000,000까지 표현할 수 있다는 말과 같다.
+// 이 값은 (2^10)^100000이므로 대략 10^300000이 된다.
+// longlong이나 int형으로 표현할 수 없는 범위이므로
+// string을 사용해서 입력을 받도록한다.
+
 #include <iostream>
-#include <sstream>
-#include <vector>
-// 공백문자를 건너뛰고 숫자를 저장하는 방법
-// 
-// 노가다 없이 최대공약수 구하기
-// 유클리드 호제법 사용
-#include <string>
 using namespace std;
-
-
-int GCD(int s, int b)
-{
-	if (b >= s)
-	{
-		if (b % s == 0)
-		{
-			return s;
-		}
-		else
-		{
-			return GCD(s, b % s);
-		}
-	}
-	else
-	{
-		if (s % b == 0)
-		{
-			return b;
-		}
-		else
-		{
-			return GCD(b, s % b);
-		}
-	}
-}
-
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
-	// 양의 정수 n개가 주어졌을때 가능한 모든 쌍의 GCD(Greatest Common Division) 합 구하기
-	// 최대공약수 구하기
-	// 2 4 2 4  10 10 10  10 20  10
-	int t;
-	cin >> t;
 
-	vector<int> vec;
-	vector<int> gcd;
+	string input;
+	cin >> input;
 
-	while (t > 0)
+	//8진법으로 바꾸려면 앞에서부터 세자리씩 끊어 바꾸면 된다.
+	//input % 3을 확인하고, 0이 나오지 않을경우 앞에 0을 넣어서
+	//나머지가 0이 되도록 만든다.
+	while (input.size() % 3 != 0)
 	{
-		vec.clear();
-
-		int n;
-		cin >> n;
-
-		for (int i = 0; i < n; i++)
-		{
-			int input;
-			cin >> input;
-			vec.push_back(input);
-		}
-		//1, 000, 000 가 100개 주어질경우
-		//100!이므로 longlong으로 만들어준다.
-		//총 합의 범위는 10*99/2*1,000,000 이므로 49억이다??
-		long long total = 0;
-		for (int i = 0; i < vec.size(); i++)
-		{
-			for (int j = i + 1; j < vec.size(); j++)
-			{
-				total += GCD(vec[i], vec[j]);
-			}
-		}
-		//3 125 15 25
-		cout << total;
-		if (t > 1)
-		{
-			cout << "\n";
-		}
-		t--;
+		input = '0' + input;
 	}
-	
-
+	//이제 3자리씩 8진법으로 읽어준다. 
+	for (int i = 0; i < input.size(); i += 3)
+	{
+		cout << (input[i] - '0') * 4 + (input[i + 1] - '0') * 2 + (input[i + 2] - '0') * 1;
+	}
 
 	return 0;
 }
