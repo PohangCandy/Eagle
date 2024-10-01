fcecbe8dcc2f3c22c6400681367e9782334d751d
PohangCandy <rlcks777@naver.com>
2024-10-01 18:16:46 +0900

    2089

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,5 +1,20 @@
+// n진법 연산은 주어진 input을 n으로 나눈 나머지를 출력한 결과로 나타낸다.
+// -n진법의 경우 나머지가 -가 나올 수 있다.
+// -로 나온 나머지를 양수로 바꾸려면 어떻게 해야할까?
+// -2진법의 경우, 
+// -5는 -2 * 3 + 1 또는 -2 * 2 - 1 두 가지 표현될 수 있다.
+// 컴파일러는 이럴때 어떻게 출력할까?
+// c++ 컴파일러는 2를 출력한다.
+// 5는 -2 * -2 + 1 또는 -2 * -3 -1 두 가지로 표현될 수 있다.
+// c++ 컴파일러는 -2를 출력한다.
+// 나머지를 양수로 표현하기 위해서
+// -5는 -6 + 1으로 5는 4 + 1로 표현되어야 한다.
+// 그러므로 홀수인 경우 -1을 해서 짝수로 만든 후 연산하고 
+// 나머지 값으로 1을 출력해준다.
+// 
+
 #include <iostream>
-#include <deque>
+#include <stack>
 using namespace std;
 
 int main()
@@ -7,54 +22,36 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-	//수의 길이가 333,334를 넘지 않는다.
-	//8진수 이므로 2^1,000,000 = 10^300,000이므로
-	//string으로 입력을 받아준다.
+	//결과를 저장해줄 벡터
+	stack<int> res;
+	int N;
+	cin >> N;
 
-	string input;
-	cin >> input;
-
-	deque<int> res;
-
-	for (int i = 0; i < input.size(); i++)
+	if (N == 0)
 	{
-		if (input[i] - '0' >= 4)
+		cout << 0;
+		return 0;
+	}
+
+	while (N != 0)
+	{
+		if ((N % -2) == 0)
 		{
-			res.push_back(1);
-			input[i] -= 4;
+			res.push(0);
+			N /= -2;
 		}
 		else
 		{
-			res.push_back(0);
-		}
-		if (input[i] - '0' >= 2)
-		{
-			res.push_back(1);
-			input[i] -= 2;
-		}
-		else
-		{
-			res.push_back(0);
-		}
-		if (input[i] - '0' >= 1)
-		{
-			res.push_back(1);
-			input[i] -= 1;
-		}
-		else
-		{
-			res.push_back(0);
+			res.push(1);
+			N -= 1;
+			N /= -2;
 		}
 	}
 
-	while (res.front() == 0)
+	while (!res.empty())
 	{
-		res.pop_front();
-	}
-
-	for (auto& a : res)
-	{
-		cout << a;
+		cout << res.top();
+		res.pop();
 	}
 
 	return 0;
