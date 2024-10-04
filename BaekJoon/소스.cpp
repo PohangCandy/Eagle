42de3601d6b964958792b0bfd5c4303b440b6e77
PohangCandy <rlcks777@naver.com>
2024-10-04 18:01:57 +0900

    14002

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,33 +1,51 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-// 수열의 순서가 유지된 채로 가장 긴 부분수열의 길이를 구해야한다.
-// {10,1,2,3,4,5,20}에서 가장 긴 부분 수열이 5가 나와야된다.
-// {10,1,2,3,11,12,13,14}에서 가장 긴 부분 수열이 5가 나와야된다.
-// 만약에 최장 부분수열의 중간 값이 겹치는 경우
-// {10,1,2,20,21,22,23}
-// 중간 값의 앞에서 어떤 수열이 더 많은 요소를 가지고 있냐에 달려있다.
-// {7,8,9,10,1,2,3,4,5,10}
-// input이 들어오면 1을 담고
-// 1 ~ (input - 1)이 가지고 있는 값과 비교하여
-// 가장 큰 값을 담는다.
-// arr[input] = 1;
-// for(int i = 1; i <= input - 1; i++
-// {
-//	  if(arr[i] > arr[input]) arr[input] = arr[i];
-// }
-// 1000개의 수와 1000이 주어진다면
-// 10^6이므로 1초 미만으로 실행된다.
+//LIS를 구할때, 자신이 마지막으로 포함시키는 수열에 대한 정보를 가지고 있어야한다.
+// 10 20 10 30 20 50 일 경우
+// 10	10
+// 20	10,20 
+// 10	10 
+// 30	10,20,30
+// 20	10,20
+// 50	10,20,30,50
 
-void RR(int arr[], int N)
+// 10 20 1 2 3 4 5 20
+// 10	10
+// 20	10,20
+// 1	1
+// 2	1,2
+// 3	1,2,3
+// 4	1,2,3,4
+// 20	1,2,3,4
+// 입력보다 작은 수 중 LIS가 가장 큰 친구의 수열
+//  + push_back(입력)을 저장한다.
+// 
+// vector<int> 자료형을 저장하는 
+// 배열를 만들어서 저장하는 공간에 담아본다.
+// 그냥 수열만 담아주고, 해당 수열의 사이즈로 비교해도 가능하다.
+
+
+void RR(vector<int> arr[], int N)
 {
-	arr[N] = 1;
+	if (arr[N].size() == 0)
+	{
+		arr[N].push_back(N);
+	}
+
 	for (int i = 1; i <= (N - 1); i++)
 	{
-		if (arr[i] >= arr[N]) arr[N] = arr[i] + 1;
+		if (arr[i].size() >= arr[N].size())
+		{
+			//LIS의 수열을 초기화시킨다.
+			arr[N].clear();
+			arr[N] = arr[i];
+			arr[N].push_back(N);
+		}
 	}
 }
 
-int arr[1000];
+vector<int> test[1001];
 
 int main()
 {
@@ -40,16 +58,26 @@ int main()
 	{
 		int input;
 		cin >> input;
-		RR(arr, input);
+		RR(test, input);
 	}
 
 	int max = 0;
+	int index = 1;
 	for (int i = 1; i <= 1000; i++)
 	{
-		if (arr[i] > max) max = arr[i];
+		if (test[i].size() > max)
+		{
+			max = test[i].size();
+			index = i;
+		}
 	}
 
-	cout << max;
+	cout << test[index].size() << "\n";
+	for (auto& a : test[index])
+	{
+		cout << a << " ";
+	}
+
 
 	return 0;
 }
