d2a6aeb8faa643b5a186ef415433618a6a956220
PohangCandy <rlcks777@naver.com>
2024-10-07 10:43:02 +0900

    solvig 2225

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -27,13 +27,45 @@ using namespace std;
 
 int arr[201][201];
 
+int RR(int arr[][201], int input, int num)
+{
+	// 0 = 0
+	arr[0][1] = 1;
+	for (int i = 1; i <= input; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			if (j == 1)
+			{
+				arr[i][j] = 1;
+				continue;
+			}
+			int total = 0;
+			for (int k = i; k >= 0; k--)
+			{
+				total += arr[k][j - 1];
+			}
+			arr[i][j] = total;
+			//arr[6][1] = 1
+			//arr[6][2] = 0,arr[6][1] + 1,arr[5][1] + ... + 6,arr[0][1]
+			//arr[6][3] = 0,arr[6][2] + 1,arr[5][2] + ... + 6,arr[0][2]
+			//arr[i][j] = arr[i-j][j]
+		}
+	}
+	
+
+	return arr[input][num];
+}
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
+	int N, K;
+	cin >> N >> K;
 
+	cout << RR(arr, N , K);
 
 	return 0;
 }
