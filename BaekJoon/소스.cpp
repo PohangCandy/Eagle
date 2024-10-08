3cbd227eec197d8e276fe806ae87cda4347553db
PohangCandy <rlcks777@naver.com>
2024-10-08 15:52:44 +0900

    9465

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,34 +1,45 @@
 #include <iostream>
 using namespace std;
-// 우리가 최대 100000개라
-// 들어갈 수 있는 최대 사자는 총 50000마리이다.
-// 배열을 arr[100001][50001]이면 크다고 오류난다.
+// n = 1일때,
+// 아무것도 고르지 않을 경우의 인덱스 0,
+// 위쪽을 고르는 경우 인덱스 1,
+// 아래쪽을 고르는 경우 인덱스 2라 한다.
+// 아무것도 고르지 않았을 때, 가격의 최댓값
+// arr[1][0] = 0;
+// arr[1][1] = 50;
+// arr[1][2] = 30;
+// arr[2][0] = max(arr[1][1], arr[1][2])
+// arr[2][1] += arr[1][2]
+// arr[2][2] += arr[1][1]
+// arr[3][0] = max(arr[2][1],arr[2][2])
+// arr[3][1] += max(arr[2][2], arr[2][0])
+// arr[3][2] += max(arr[2][1], arr[2][0])
 
-// 2 * n 개의 우리에서
-// n번째 행에 사자가 들어있거나, 한마리만 들어있는 경우를 배열로 나타낸다.
-// 사자가 첫번째 우리에 없는 경우 [n][0]
-// 사자 한마리가 첫번째 우리 왼쪽에 있는 경우 [n][1]
-// 사자 한마리가 첫번째 우리 오른쪽에 있는 경우 [n][2]
+// 점화식
+// arr[n][0] = max(arr[n-1][1],arr[n-1][2])
+// arr[n][1] += max(arr[n-1][2],arr[n-1][0])
+// arr[n][2] += max(arr[n-1][1],arr[n-1][0])
+// 
+// n * 2개의 스티커에서 가겨이 최대가 되는 값
+// return max(arr[n][1], arr[n][2])
 
 int arr[100001][3];
 
 int RR(int arr[][3], int n)
 {
-	arr[1][0] = 1;
-	arr[1][1] = 1;
-	arr[1][2] = 1;
+	arr[1][0] = 0;
+
 	for (int i = 2; i <= n; i++)
 	{
-		//i번째 행에 사자가 없는 경우의 수는
-		//i-1번째 행에 사자가 없거나
-		//i-1번째 행에 사자가 왼쪽에 있거나
-		//i-1번째 행에 사자가 오른쪽에 있는 경우의 수의 총합이다.
-		arr[i][0] = (arr[i - 1][0] + arr[i - 1][1] + arr[i - 1][2]) % 9901;
-		arr[i][1] = (arr[i - 1][0] + arr[i - 1][2]) % 9901;
-		arr[i][2] = (arr[i - 1][0] + arr[i - 1][1]) % 9901;
+		//초기화
+		arr[i][0] = 0;
+
+		arr[i][0] = max(arr[i - 1][1], arr[i - 1][2]);
+		arr[i][1] += max(arr[i - 1][2], arr[i - 1][0]);
+		arr[i][2] += max(arr[i - 1][1], arr[i - 1][0]);
 	}
 
-	return (arr[n][0] + arr[n][1] + arr[n][2]) % 9901;
+	return max(arr[n][1], arr[n][2]);
 }
 
 int main()
@@ -36,9 +47,30 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-	int N;
-	cin >> N;
-	cout << RR(arr, N);
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; i++)
+	{
+		int n;
+		cin >> n;
+		//스티커 윗줄의 가격
+		for (int i = 1; i <= n; i++)
+		{
+			int input;
+			cin >> input;
+			arr[i][1] = input;
+		}
+		//스티커 아랫줄의 가격
+		for (int j = 1; j <= n; j++)
+		{
+			int input;
+			cin >> input;
+			arr[j][2] = input;
+		}
+
+		cout << RR(arr, n) << "\n";
+	}
+
 
 	return 0;
 }
