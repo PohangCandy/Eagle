b6c97951def93d767eb74d6d5b79795bcd0ba57f
PohangCandy <rlcks777@naver.com>
2024-10-10 14:47:18 +0900

    still soving

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -15,19 +15,38 @@ using namespace std;
 //18 11 16 20
 
 //삼각형을 이루는 총 항의 개수 = n*(n+1)/2
-//n번째 줄의 가장 마지막 항arr[n*(n+1)/2]에서
+// 
+//가장 마지막 항arr[n*(n+1)/2]에서
 //삼각형의 윗 꼭지점에 접근하려면
-//arr[n*(n+1)/2 - n] = arr[n*(n+1)/2 - n] + max(arr[n*(n+1)/2],arr[n*(n+1)/2 - 1])
+//arr[n*(n+1)/2 - n] += max(arr[n*(n+1)/2],arr[n*(n+1)/2 - 1])
 //이 작업을 각 줄의 두번째 항까지 반복한다.
 //그 다음 윗 줄로 올라간다.
 //최종적으로 arr[1]에 남은 값이 최댓값이 된다.
+
+int arr[500 * 501 / 2 + 1];
+
+int RR(int arr[], int n)
+{
+	for (n; n > 1; n--)
+	{
+		arr[n * (n + 1) / 2 - n] += max(arr[n * (n + 1) / 2], arr[n * (n + 1) / 2 - 1]);
+	}
+}
+
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		arr[i] = 0;
+	}
 
+	cout << RR(arr, n);
 
 	return 0;
 }
