043bc798e268f247ee42558e011a5bbb4c2b301f
jaeho kim <rlawogh1005@gmail.com>
2024-10-17 14:42:30 +0900

    solving 3085

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,78 +1,122 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-
 using namespace std;
 
-//아홉 난쟁이중 일곱명의 난쟁이 합이 100
-//두명의 난쟁이를 제외했을때 합이 100이 되는지 조사한다.
+//애니팡
+
+/*
+힌 줄 바꿔서, 가장 긴 사탕이 있는 줄 출력하기
+사탕을 배열에 담은 뒤에, 뒤에 있거나 아래에 있는 경우, count를 증가시킨다.
+max가 되는 count 값을 저장시킨다.
+
+먼저 사탕을 최대로 넣을 수 있는 배열을 만든다.
+입력한 입력 값만큼, 가로 배열과 세로 배열의 값을 증가시킨다.
+
+색상 4개에 대한 배열을 따로만든다.
+
+i,j번째 요소에서
+가로와 세로 줄을 계속 세면되지 않을까?
+가로로 증가시키면서 최대 몇개가 연속될 수 있는지
+조사하고
+세로로 증가시키면서 최대 몇개가 될 수 있는지
+조사해본다.
+
+80%에서 멈춘다.
+반례가 발생하고 있기때문에
+예외가 있는지 모든 사항을 고려해본다.
+
+p p p c p
+c c p c c
+*/
+
+char anipang[51][51];
+
+//이미 한 번 바꿨을 경우와, 한 번도 안 바꿨을 경우를 고려해준다.
+int arr[2];
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-	vector<int> hobit;
+	int N;
+	cin >> N;
 
-	for (int i = 0; i < 9; i++)
+	for (int i = 1; i <= N; i++)
 	{
-		int input;
-		cin >> input;
-		hobit.push_back(input);
+		for (int j = 1; j <= N; j++)
+		{
+			char c;
+			cin >> c;
+			anipang[i][j] = c;
+		}
 	}
 
-	sort(hobit.begin(), hobit.end());
+	int M = 1;
 
-	//for (int i = 0; i < hobit.size(); i++)
-	//{
-	//	cout << hobit[i] << "\n";
-	//}
+	//가로에 대해서 먼저 조사
 
-
-	int fake_one = 0;
-	int fake_two = 0;
-
-	for (int i = 0; i < hobit.size(); i++)
+	for (int i = 1; i <= N; i++)
 	{
-		int total = 0;
-
-		for (int j = i + 1; j < hobit.size(); j++)
+		for (int j = 1; j <= N; j++)
 		{
-			total = 0;
-			for (int k = 0; k < hobit.size(); k++)
+			arr[0] = 1;
+			arr[1] = 1;
+			char basicWord = anipang[i][j];
+			for (int k = j + 1; k <= N; k++)
 			{
-				if (k == i || k == j)
+				if (basicWord == anipang[i][k])
 				{
-					continue;
+					arr[0]++;
+					arr[1]++;
 				}
-				else {
-					total += hobit[k];
+				else if (basicWord == anipang[i + 1][k] || basicWord == anipang[i - 1][k])
+				{
+					arr[1] = arr[0] + 1;
+					arr[0] = 0;
 				}
+				else
+				{
+					arr[0] = 0;
+					arr[1] = 0;
+				}
+				M = max({M,arr[0], arr[1]});
 			}
-			if (total == 100)
-			{
-				fake_one = j;
-				break;
-			}
-		}
-
-		if (total == 100)
-		{
-			fake_two = i;
-			break;
 		}
 	}
 
-	for (int i = 0; i < hobit.size(); i++)
+	//세로에 대한 조사
+	for (int i = 1; i <= N; i++)
 	{
-		if (i == fake_one || i == fake_two)
+		for (int j = 1; j <= N; j++)
 		{
-			continue;
-		}
-		else {
-			cout << hobit[i] << "\n";
+			arr[0] = 1;
+			arr[1] = 1;
+			char basicWord = anipang[j][i];
+			for (int k = j + 1; k <= N; k++)
+			{
+				if (basicWord == anipang[k][i])
+				{
+					arr[0]++;
+					arr[1]++;
+				}
+				else if (basicWord == anipang[k][i+1] || basicWord == anipang[k][i-1])
+				{
+					arr[1] = arr[0] + 1;
+					arr[0] = 0;
+				}
+				else
+				{
+					arr[0] = 0;
+					arr[1] = 0;
+				}
+
+				M = max({ M,arr[0], arr[1] });
+			}
 		}
 	}
+
+	cout << M;
 
 
 	return 0;
