8ff7ef51d193e7c24fa99f20b961bfff870eeee5
jaeho kim <rlawogh1005@gmail.com>
2024-10-17 18:53:48 +0900

    still solving

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -12,7 +12,7 @@ max가 되는 count 값을 저장시킨다.
 먼저 사탕을 최대로 넣을 수 있는 배열을 만든다.
 입력한 입력 값만큼, 가로 배열과 세로 배열의 값을 증가시킨다.
 
-색상 4개에 대한 배열을 따로만든다.
+색상 4개에 대한 배열을 따로 만든다.
 
 i,j번째 요소에서
 가로와 세로 줄을 계속 세면되지 않을까?
@@ -25,8 +25,39 @@ i,j번째 요소에서
 반례가 발생하고 있기때문에
 예외가 있는지 모든 사항을 고려해본다.
 
-p p p c p
-c c p c c
+예외적인 경우가 있다.
+
+p p p c p p p p
+z y z z y z y z
+위와 같은 경우 
+p p p이후에 오는 c,p를 교환하는 경우
+p p p (p c) p p p 로 교체하면 앞 줄은 p가 4개, 뒷 줄은 3개가 된다.
+p p (c p) p p p p 로 교체하면 앞 줄은 p가 3개, 뒷 줄은 5개가 된다.
+
+가로를 검사할때
+for(int i  = 0 ; i < N; i++)
+for(int j = 0 ; i < N ; j++)
+for(int k = j+1; k < N; k++)
+[i][j]번째 요소와 [i][k]번째 요소를 비교했을때
+
+[i][j] == [i][k]라면, 연속된 수를 저장하는 배열에 담아준다.
+arr[0] ++;
+arr[1]++, 체인지를 한번 선언한 경우의 뱌열도 똑같이 증가한다.
+
+[i][j] != [i][k]리면,
+자리 체인지를 통해 연속될 수 있는지 먼저 알아본다.
+
+1. 상,하 체인지로 연속되는 경우 
+[i][j] == [i+1][k] || [i][j] == [i-1][k]
+arr[1] = arr[0] + 1; 지금까지 연속된 합 + 체인지를 통해 더해진 1
+연속된 수를 저장하는 배열은 초기화
+arr[0] = 0 ;
+
+2. 좌, 우 체인지로 연속되는 경우
+[i][j] == [i][k+1] == [i][k-1]인 경우
+k = 1일때
+P C P 같은 경우를 말한다.
+
 */
 
 char anipang[51][51];
@@ -68,17 +99,26 @@ int main()
 				if (basicWord == anipang[i][k])
 				{
 					arr[0]++;
-					arr[1]++;
+					arr[1]++; 
 				}
 				else if (basicWord == anipang[i + 1][k] || basicWord == anipang[i - 1][k])
 				{
 					arr[1] = arr[0] + 1;
 					arr[0] = 0;
 				}
+				else if (k + 1 < N && basicWord == anipang[i][k+2] && anipang[i][k-1] == anipang[i][k + 1])
+				{
+					arr[1] = arr[0] + 1;
+					M = max({ M,arr[0], arr[1]});
+					arr[1] = 1;
+					arr[0] = 0;
+				}
 				else
 				{
-					arr[0] = 0;
+					//p p p c p p p p
+					//c c p c c p
 					arr[1] = 0;
+					arr[0] = 0;
 				}
 				M = max({M,arr[0], arr[1]});
 			}
@@ -105,10 +145,17 @@ int main()
 					arr[1] = arr[0] + 1;
 					arr[0] = 0;
 				}
+				else if (k + 1 < N && basicWord == anipang[k + 1][i])
+				{
+					arr[1] = arr[0] + 1;
+					M = max({ M,arr[0], arr[1] });
+					arr[1] = 1;
+					arr[0] = 0;
+				}
 				else
 				{
-					arr[0] = 0;
 					arr[1] = 0;
+					arr[0] = 0;
 				}
 
 				M = max({ M,arr[0], arr[1] });
