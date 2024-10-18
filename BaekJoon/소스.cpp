3f51aab6e146479d1aeedeab4c49f736b35be232
PohangCandy <rlcks777@naver.com>
2024-10-18 18:49:58 +0900

    1107

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,193 +1,66 @@
+//to_string을 사용해서 좀 더 간단한 코드로 작성해본다.
 #include <iostream>
+#include <string>
 using namespace std;
 
-// 리모컨 버튼 최소로 눌러서 원하는 채널 N으로 이동하기
-// 고장난 버튼을 제거하고 최소 이동 경로 찾기
-// 시작 지점은 100
+int remocon[10] = {0};
 
-//  80000
-//	77777	   (5)
-//	 2223	(2223)
+bool check(int n)
+{
+	//수를 string으로 변환해서 
+	//자릿수에 해당하는 수의 고장여부를 
+	//인덱스를 통해 간단하게 체크한다.
+	string s = to_string(n);
+	for (int i = 0; i < s.length(); i++)
+	{
+		//고장난 버튼인 경우 
+		if (remocon[s[i] - '0'] == 1)
+		{
+			return false;
+		}
+	}
 
-//1. 입력받은 N에 남아있는 숫자 버튼을 가지고 최대한 가까이 접근하기
-//2. 위아래 버튼으로 입력받은 수에 접근하기
-
-// 이동하고자 하는 채널이 n이라면
-// 1. 위아래버튼으로 접근하는 경우의 수는 n - 100이다.
-// 
-// 2. 최대한 가까이 접근한 수가 s일때
-// 위아래 버튼을 사용하는 경우의 수는 n-s이다.
-//
-// max(n - s + s의 자릿수, n - 100)을 한 결과가
-// 최종 답이 된다.
-
-// n > s인 경우
-// 더 큰 자릿수가 되거나, 같은 자릿수 중에 나올 수 있는 가장 최소한으로 큰 값이 앞자리에 나온다.
-
-//구해야하는 값은 두가지이다.
-// 입력받은 수에 가장 근접한 두 수를 구한다.
-// 하나의 수는 입력받은 값보다 작은 수 중에서 구한다.
-// 하나의 수는 입력받은 값보다 큰 수 중에서 구한다.
-
-//리모컨에 있는 버튼의 고장 유무를 나타낸다. 
-bool remocon[10];
-
-int result = 0;
+	return true;
+}
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-	//기존의 리모컨은 모두 작동가능
-	for (auto& a : remocon)
-	{
-		a = true;
-	}
-
 	int N;
 	cin >> N;
-	
-	int numOfBrakedKeyboard;
-	cin >> numOfBrakedKeyboard;
-	for (int i = 0; i < numOfBrakedKeyboard; i++)
+
+	int broken;
+	cin >> broken;
+	for (int i = 0; i < broken; i++)
 	{
-		int remoconNum;
-		cin >> remoconNum;
-		remocon[remoconNum] = false;
+		int input;
+		cin >> input;
+		remocon[input] = 1;
 	}
+	//먼저 그냥 위아래 버튼을 움직이는 경우를 구한다.
+	int result = abs(N - 100);
 
-	//100번 채널에서 시작했을때
-	//위,아래로 움직이는 버튼만 가지고 찾는 경우의 수
-	result = abs(N - 100);
-
-	//N보다 적은 값 중 리모컨으로 접근 할 수 있는 최댓값
-	for(int i = N; i >= 0; i--)
+	// 최대로 입력될 수 있는 경우가 500000이므로
+	// 리모컨이 고장날 경우를 고려해 최대 999999를 입력받을 수 있다.
+	// i 가 500000 이고  키보드가 9만 멀쩡할 경우
+	for (int i = 0; i <= 999999; i++)
 	{
-		int c = i;
-		//예외처리 필요
-		// i == 0이고 0이 고장난 경우
-		// 즉,N 아래에서 접근 할 수 있는 수가 없는 경우
-		// 즉시 break를 걸어준다.
-		// 0이 고장나지 않았을 경우
-		// 
-		if (c == 0)
-		{
-			if (remocon[0] == false)
-			{
-				break;
-			}
-			// 접근 가능한 수가 0이라면
-			// 0에서 위아래로 이동하는 경우와
-			// 현재 결과를 비교해준다.
-			else
-			{
-				//0에서 이동하는데 걸리는 위아래 버튼 N번
-				//0을 입력하므로 +1
-				result = min(result, N + 1);
-			}
-		}
-		while (c != 0)
-		{
-			//고장난 리모컨이 아닐경우 계속해서 누른다.
-			if (remocon[c % 10])
-			{
-				c /= 10;
-			}
-			else
-			{
-				break;
-			}
-		}
-
-		//고장난 리모컨일 경우
-		if (c != 0)
+		//i에 해당하는 값의 키보다가 고장나지 않았는지 체크한다.
+		if (!check(i))
 		{
 			continue;
 		}
-		//모든 리모컨번호가 살아있는 번호인 경우
-		else
-		{
-			//자릿수를 구해준다.
-			int count = 0;
-			while (i != 0)
-			{
-				i /= 10;
-				count++;
-			}
 
-			//자릿수와 위아래 이돟하는 경우의 수를 더한 합과
-			//이전 결과 중 더 작은 값으로 결정한다.
-			result = min(result, abs(N - i) + count);
-			//가장 첫번째로 구한 값이 가장 근접한 값이므로
-			//break를 통해 for문을 빠져나와도 상관없다.
-			break;
-		}
+		//사용된 버튼이 모두 고장나지 않았을 경우
+		//해당 채널에서의 거리를 구한다.
+		string s = to_string(i);
+		int count = abs(N - i) + s.length();
+		result = min(result,count);
 	}
 
-	//N보다 큰 값 중 리모컨으로 접근 할 수 있는 최댓값
-	for (int i = N; i <= 500000; i++)
-	{
-		int c = i;
-		//예외처리 필요
-		// i == 500000이고 0
-		// 즉,N 아래에서 접근 할 수 있는 수가 없는 경우
-		// 즉시 break를 걸어준다.
-		// 0이 고장나지 않았을 경우
-		// 
-		if (c == 0)
-		{
-			if (remocon[0] == false)
-			{
-				break;
-			}
-			// 접근 가능한 수가 0이라면
-			// 0에서 위아래로 이동하는 경우와
-			// 현재 결과를 비교해준다.
-			else
-			{
-				//0에서 이동하는데 걸리는 위아래 버튼 N번
-				//0을 입력하므로 +1
-				result = min(result, N + 1);
-			}
-		}
-		while (c != 0)
-		{
-			//고장난 리모컨이 아닐경우 계속해서 누른다.
-			if (remocon[c % 10])
-			{
-				c /= 10;
-			}
-			else
-			{
-				break;
-			}
-		}
-
-		//고장난 리모컨일 경우
-		if (c != 0)
-		{
-			continue;
-		}
-		//모든 리모컨번호가 살아있는 번호인 경우
-		else
-		{
-			//자릿수를 구해준다.
-			int count = 0;
-			while (i != 0)
-			{
-				i /= 10;
-				count++;
-			}
-
-			//자릿수와 위아래 이돟하는 경우의 수를 더한 합과
-			//이전 결과 중 더 작은 값으로 결정한다.
-			result = min(result, abs(N - i) + count);
-			//가장 첫번째로 구한 값이 가장 근접한 값이므로
-			//break를 통해 for문을 빠져나와도 상관없다.
-			break;
-		}
-	}
+	cout << result;
 
 	return 0;
 }
