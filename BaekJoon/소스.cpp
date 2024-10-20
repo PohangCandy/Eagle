030c97fbfeadb14a3ebccb16e297af79c4d4c087
PohangCandy <rlcks777@naver.com>
2024-10-21 01:44:12 +0900

    solving 14500

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,26 +1,24 @@
-//to_string을 사용해서 좀 더 간단한 코드로 작성해본다.
 #include <iostream>
-#include <string>
+#include <algorithm>
 using namespace std;
 
-int remocon[10] = {0};
+int Tile[510][510] = { 0 };
 
-bool check(int n)
+int whenUptile(int i, int j)
 {
-	//수를 string으로 변환해서 
-	//자릿수에 해당하는 수의 고장여부를 
-	//인덱스를 통해 간단하게 체크한다.
-	string s = to_string(n);
-	for (int i = 0; i < s.length(); i++)
-	{
-		//고장난 버튼인 경우 
-		if (remocon[s[i] - '0'] == 1)
-		{
-			return false;
-		}
-	}
-
-	return true;
+	return Tile[i][j] + max({ Tile[i][j + 1],Tile[i][j - 1],Tile[i - 1][j] });
+}
+int whenDowntile(int i, int j)
+{
+	return Tile[i][j] + max({ Tile[i][j + 1],Tile[i][j - 1],Tile[i + 1][j] });
+}
+int whenLefttile(int i, int j)
+{
+	return Tile[i][j] + max({ Tile[i - 1][j],Tile[i + 1][j],Tile[i][j - 1] });
+}
+int whenRighttile(int i, int j)
+{
+	return Tile[i][j] + max({ Tile[i - 1][j],Tile[i + 1][j],Tile[i][j + 1] });
 }
 
 int main()
@@ -28,39 +26,53 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-	int N;
-	cin >> N;
+	int N, M;
+	cin >> N >> M;
 
-	int broken;
-	cin >> broken;
-	for (int i = 0; i < broken; i++)
+	for (int i = 10; i < N + 10; i++)
 	{
-		int input;
-		cin >> input;
-		remocon[input] = 1;
-	}
-	//먼저 그냥 위아래 버튼을 움직이는 경우를 구한다.
-	int result = abs(N - 100);
-
-	// 최대로 입력될 수 있는 경우가 500000이므로
-	// 리모컨이 고장날 경우를 고려해 최대 999999를 입력받을 수 있다.
-	// i 가 500000 이고  키보드가 9만 멀쩡할 경우
-	for (int i = 0; i <= 999999; i++)
-	{
-		//i에 해당하는 값의 키보다가 고장나지 않았는지 체크한다.
-		if (!check(i))
+		for (int j = 10; j < M + 10; j++)
 		{
-			continue;
+			int input;
+			cin >> input;
+			Tile[i][j] = input;
 		}
-
-		//사용된 버튼이 모두 고장나지 않았을 경우
-		//해당 채널에서의 거리를 구한다.
-		string s = to_string(i);
-		int count = abs(N - i) + s.length();
-		result = min(result,count);
 	}
 
-	cout << result;
+	int total = 0;
+
+	for (int i = 10; i < N + 10; i++)
+	{
+
+		for (int j = 10; j < M + 10; j++)
+		{
+			int up = Tile[i - 1][j] + max({ whenUptile(i - 2, j),whenRighttile(i - 1,j + 1),whenLefttile(i - 1,j - 1) });
+			int down = Tile[i + 1][j] + max({ whenDowntile(i + 2, j),whenRighttile(i + 1,j + 1),whenLefttile(i + 1,j - 1) });
+			int right = Tile[i][j + 1] + max({ whenRighttile(i, j + 2),whenUptile(i - 1,j + 1),whenDowntile(i + 1,j + 1) });
+			int left = Tile[i][j - 1] + max({ whenLefttile(i, j - 2),whenUptile(i - 1,j - 1),whenDowntile(i + 1,j - 1) });
+
+			int m = Tile[i][j] + max({ up,down,right,left });
+			total = max(m, total);
+
+			//한 개의 정사각형을 먼저 픽하는 경우
+			up = Tile[i - 1][j] + max({ whenDowntile(i + 1, j),whenRighttile(i,j + 1),whenLefttile(i,j - 1) });
+			down = Tile[i + 1][j] + max({ whenUptile(i - 1, j),whenRighttile(i,j + 1),whenLefttile(i,j - 1) });
+			right = Tile[i][j + 1] + max({ whenLefttile(i,j - 1), whenUptile(i - 1, j),whenDowntile(i + 1, j) });
+			left = Tile[i][j - 1] + max({ whenRighttile(i,j + 1), whenUptile(i - 1, j),whenDowntile(i + 1, j) });
+			m = Tile[i][j] + max({ up,down,right,left });
+			total = max(m, total);
+
+			//두 개의 정사각형을 먼저 픽하는 경우
+			up = Tile[i - 1][j];
+			down = Tile[i + 1][j];
+			right = Tile[i][j + 1];
+			left = Tile[i][j - 1];
+			m = Tile[i][j] + max({ up + down + right,up + down + left,up + right + left,down + right + left });
+			total = max(m, total);
+		}
+	}
+
+	cout << total;
 
 	return 0;
 }
