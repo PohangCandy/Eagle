6f1035e2615193e799a5cfbf830012a8e15ec544
PohangCandy <rlcks777@naver.com>
2024-10-25 18:50:58 +0900

    11723

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,103 +1,98 @@
-//아이디어
-//개쩌는 규칙을 찾아냈다.
-// 그냥 마지막 노드 i부터 방문을 해제한다.
-//  방문하지 않은 노드 중에
-//  해제한 노드보다 더 큰 값이 있는지 체크한다.
-// 있으면 해당 노드를 벡터에 담고, 방문 체크한 후
-//	방문하지 않은 노드를 작은 순서로 담는다.
-// 없으면 i-1 노드의 방문을 해제하고 
-//  방문하지 않은 노드 중에 
-//  해제한 노드보다 더 큰 값이 있는지 체크한다.
+// 아이디어
+// 집합을 이용해서 연산 구현하면 될 것 같은데?
 // 
-//시간복잡도
-// 10^4^2 = 10^8
+// 시간복잡도
+//  N까지 반복
+//  집합의 find 함수
+// 집합에 들어오는 값은 20밖에 안되는데 시간초과가 왜 걸리지?
 // 
-//메모리
-// 입력된 수열 저장할 벡터 int * 10001
-//자료형
-// int, 최대 10000
+// 지우거나, 추가한 후에 정렬시키는 작업이 오래걸려서 그런가?
+// map, set은 요소를 줄이거나 늘리는 작업을 할 때
+// 자동으로 정렬시켜주지만 시간을 많이 잡아먹는다.
+// 
+// 
+// 메모리
+//  최대 int * 20
+// 자료형
+//  최대 20, int
 
 #include <iostream>
-#include <vector>
+#include <set>
 using namespace std;
-
-int N;
-vector<int> vec;
-
-int visited[10001];
-
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
+	cout.tie(0);
 
-	cin >> N;
+	set<int> st;
+	set<int> all_st;
 
-	for (int i = 1; i <= 10000; i++)
+	for (int i = 1; i <= 20; i++)
 	{
-		//모든 배열의 수를 방문한 상태로 초기화 해둔다.
-		visited[i] = 1;
+		all_st.insert(i);
 	}
+
+	int N;
+	cin >> N;
+	
 
 	for (int i = 0; i < N; i++)
 	{
-		int input;
-		cin >> input;
-		vec.push_back(input);
-	}
+		string s;
+		cin >> s;
 
-	while (!vec.empty())
-	{
-		//벡터 뒤부터 해제
-		int back = vec.back();
-		visited[back] = 0;
-		vec.pop_back();
-
-		//back보다 큰 수 중 방문하지 않은 노드 찾기
-		bool find = false;
-		for (int j = back + 1; j <= N; j++)
+		if (s == "add")
 		{
-			if (j <= 10000 && 0 <= j)
+			int input;
+			cin >> input;
+			st.insert(input);
+		}
+		else if (s == "remove")
+		{
+			int input;
+			cin >> input;
+			st.erase(input);
+		}
+		else if (s == "check")
+		{
+			int input;
+			cin >> input;
+			if (st.find(input) != st.end())
 			{
-				if (visited[j] == 0)
-				{
-					find = true;
-					visited[j] = 1;
-					vec.push_back(j);
-					break;
-				}
+				cout << 1;
+				cout << "\n";
+			}
+			else
+			{
+				cout << 0;
+				cout << "\n";
 			}
 		}
-
-		if (find)
+		else if (s == "toggle")
 		{
-			for (int i = 1; i <= N; i++)
+			int input;
+			cin >> input;
+			if (st.find(input) != st.end())
 			{
-				if (!visited[i])
-				{
-					visited[i] = 1;
-					vec.push_back(i);
-				}
+				st.erase(input);
 			}
-			break;
+			else
+			{
+				st.insert(input);
+			}
 		}
-
-		//큰 수보다 작은 값 중 가장 
-	}
-
-	if (vec.empty())
-	{
-		cout << -1;
-	}
-	else
-	{
-		for (auto& a : vec)
+		else if (s == "all")
 		{
-			cout << a << " ";
+			st.clear();
+			st = all_st;
+		}
+		else if (s == "empty")
+		{
+			st.clear();
 		}
 	}
-
 
 	return 0;
 }
