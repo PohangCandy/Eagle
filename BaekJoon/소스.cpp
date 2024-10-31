fe52629666feb01ba9e3b7e85444c7487885c9c6
PohangCandy <130345776+PohangCandy@users.noreply.github.com>
2024-10-31 18:54:37 +0900

    2108 solving.cpp

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,65 +1,73 @@
-//아이디어
-// 첫 번째로 나보다 앞에 더 큰 수가 얼마나 있는지
-// 두번째로 동점자가 내 앞에 얼마나 있는지
-// 자신보다 바로 앞에 있는 수의 뒤에 몇명의 동점자가 있는지를 카운트 한다.
 #include <iostream>
-#include <queue>
 #include <vector>
 #include <algorithm>
 using namespace std;
+
+int arr[500001] = {0};
+vector<int> vec;
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-	int T;
-	cin >> T;
+	int average;
+	int middle;
+	int most;
+	int range;
 
-	for (int i = 0; i < T; i++)
+	int N;
+	cin >> N;
+
+	float sum = 0;
+	int maxi = -4000;
+	int mini = 4000;
+
+	for (int i = 0; i < N; i++)
 	{
-		int N, M;
-		cin >> N >> M;
+		int input;
+		cin >> input;
+		arr[input]++;
+		vec.push_back(N);
+		sum += input;
 
-		priority_queue<int> pq;
-		vector <int> vec;
-
-		for (int j = 0; j < N; j++)
-		{
-			int input;
-			cin >> input;
-			vec.push_back(input);
-			pq.push(input);
-		}
-
-		int bigger = 0;
-		while (!pq.empty())
-		{
-			if (vec[M] == pq.top())
-			{
-				break;
-			}
-			bigger = pq.top();
-			pq.pop();
-		}
-
-		// bigger을 벡터 내부에서 찾아서
-		// 몇번째 인덱스인지 찾아본다.
-		auto it = find(vec.begin(), vec.end(), bigger);
-
-		int count = 0;
-
-		for (it; it < vec.end(); it++)
-		{
-			count++;
-		}
-		for (int i = 0; i < M; i++)
-		{
-			count++;
-		}
-
-		cout << count << "\n";
+		mini = min(mini, input);
+		maxi = max(maxi, input);
 	}
+
+	sort(vec.begin(), vec.end());
+
+	average = round(sum / N);
+	middle = vec[(N - 1) / 2];
+
+	int count = 1;
+	vector<int> v;
+	for (int i = 1; i <= 500000; i++)
+	{
+		if (arr[i] > count)
+		{
+			v.clear();
+			v.push_back(i);
+		}
+		else if (arr[i] == count)
+		{
+			v.push_back(i);
+		}
+	}
+	if (v.size() > 1)
+	{
+		most = v[1];
+	}
+	else
+	{
+		most = v[0];
+	}
+	range = maxi - mini;
+	
+	cout << average << "\n";
+	cout << middle << "\n";
+	cout << most << "\n";
+	cout << range;
 
 	return 0;
 }
