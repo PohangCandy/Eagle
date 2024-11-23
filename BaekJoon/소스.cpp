d58001062fdbea72d234ab1eff23668967974e6d
PohangCandy <130345776+PohangCandy@users.noreply.github.com>
2024-11-23 21:08:34 +0900

    solving 2667.cpp

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -1,78 +1,26 @@
 //아이디어
-// dfs는 연결된 노드를 조사하고 
-// 순서대로 탐색하게 하면 된다.
-// bfs는 연결된 노드를 큐에 넣고,
-// 큐에 있는 노드를 탐색하게 한다.
+// 방문하지 않은 단지를 방문하면서 dfs를 한다.
+// 방문한 단지의 최대 높이를 벡터 배열에 저장한다.
 
-//벡터로 입력을 받았더니,정렬이 되지 않으므로
-//벡터 안 내용을 정렬해준다.
-
-//모든 노드가 탐색 가능하다는 전제가 없으므로
-// 길이가 최대가 되는 탐색이 결과가 되게 해준다.
-
-//시간초과가 발생한다.
-// 최대 높이를 찾을때까지 탐색을 하도록 만들어 주어서 그런듯하다.
-// dfs, bfs를 사용할때 
-// 끊어진 트리가 없다는 점을 가정하여
-// 간선의 개수로 최대 방문 가능한 정점의 개수를
-// 최대 높이로 정해본다.
-
-//N은 최대 1000
-//M은 최대 10000
 
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <algorithm>
-#include <string.h>
 using namespace std;
 
-int N, M, V;
-int maxHeight = 0;
 
-vector<int> node[1001];
+int Tile[25][25] = { 0 };
+int visited[25][25] = { 0 };
+//상하좌우
+int x[4] = { 0,0,-1,1 };
+int y[4] = { 1,-1,0,0 };
 
-queue<int> que_bfs;
-
-int visited[1001];
-
-void dfs(int s)
+void dfs(int w, int h)
 {
-	visited[s] = 1;
-	cout << s << " ";
-
-	//노드가 연결하고 있는 모든 간선을 조사한다.
-	for (int i = 0; i < node[s].size(); i++)
+	for (int i = 0; i < 4; i++)
 	{
-		if (!visited[node[s][i]])
-		dfs(node[s][i]);
-	}
-}
-
-void bfs(int s)
-{
-	visited[s] = 1;
-	que_bfs.push(s);
-
-	//큐가 비어있지 않다면
-	while (!que_bfs.empty())
-	{
-		// 큐의 프론트에 있는 값을 출력하고,
-		// 프론트와 연결된 노드를 큐에 집어넣는다.
-		cout << que_bfs.front() << " ";
-		int x = que_bfs.front();
-		que_bfs.pop();
-
-		//큐의 프론트에 있던 정점과 연결된 정점을 다시 큐에 담아준다.
-		for (int i = 0; i < node[x].size(); i++)
-		{
-			//노드가 갈 수 있는 노드를 모두 큐에 담는다.
-			if (!visited[node[x][i]])
-			{
-				visited[node[x][i]] = 1;
-				que_bfs.push(node[x][i]);
-			}
-		}
+		if (visited[w + x[i]][h + y[i]]) continue;
+		visited[w + x[i]][h + y[i]] = 1;
+		dfs(w + x[i], h + y[i]);
 	}
 }
 
@@ -81,27 +29,26 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-	cin >> N >> M >> V;
+	int N;
+	cin >> N;
 
-	for (int i = 0; i < M; i++)
+	for (int i = 0; i < N; i++)
 	{
-		int a, b;
-		cin >> a >> b;
-		node[a].push_back(b);
-		node[b].push_back(a);
+		for (int j = 0; j < N; j++)
+		{
+			cin >> Tile[i][j];
+		}
 	}
 
-	for (int i = 1; i <= N; i++)
+	for (int i = 0; i < N; i++)
 	{
-		sort(node[i].begin(), node[i].end());
+		for (int j = 0; j < N; j++)
+		{
+			if (visited[Tile[i][j]]) continue;
+			dfs(i , j);
+		}
 	}
 
-	dfs(V);
-
-	memset(visited, 0, sizeof(visited));
-	cout << "\n";
-
-	bfs(V);
 
 	return 0;
 }
