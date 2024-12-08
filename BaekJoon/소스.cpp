826c19dbc9fa1f91677b3cecbe4f96eb6a741866
PohangCandy <130345776+PohangCandy@users.noreply.github.com>
2024-12-09 08:53:17 +0900

    sing 1261.cpp

--- BaekJoon/소스.cpp
+++ BaekJoon/소스.cpp
@@ -18,57 +18,161 @@
 
 // 벽을 뚫은 횟수가 
 // 적은 탐색 경로를 우선으로 탐색한다.
-
+// 노드를 클래스로 나타내고
+// 노드에 저장할 정보를 담아본다.
 #include <iostream>
-#include <deque>
+#include <queue>
+#include <vector>
 using namespace std;
 
-int N, M;
 
-int Tile[101][101];
-int visited[101][101] = {0};
-int h[101][101] = {0};
+//if N = 4 , M = 4
+//Tile[0] -> 0,0
+//Tile[1] -> 0,1
+//Tile[2] -> 0,2
+//Tile[3] -> 0,3
+//Tile[k] -> x = k/4 , y = k%4
+//k = x * 4 + y
+
+
+class Node {
+	//좌표
+	int x;
+	int y;
+	//벽 여부
+	bool wall;
+	//방문여부
+	int visited;
+	//탐색 높이
+	int height;
+public:
+	Node(int _x, int _y,bool _w, int _v = 0, int _h = 0) : x(_x), y(_y),wall(_w), visited(_v), height(_h) {}
+
+	bool operator==(const Node n)const {
+		return ((this->x == n.x) && (this->y == n.y));
+	}
+
+	bool operator<(const Node n) const
+	{
+		return this->height < n.height;
+	}
+
+	int getheight()
+	{
+		return height;
+	}
+
+	void setheight(int h)
+	{
+		height = h;
+	}
+
+	bool ans(int _x, int _y)
+	{
+		return ((this->x == _x) && (this->y == _y));
+	}
+
+	int getxpos()
+	{
+		return x;
+	}
+
+	int getypos()
+	{
+		return y;
+	}
+
+	bool isvisited()
+	{
+		return visited;
+	}
+
+	void setvisited()
+	{
+		visited = 1;
+	}
+
+	bool isWall()
+	{
+		return wall;
+	}
+};
+
+
+int N, M;
+int ans = 10000;
+
+vector<Node> Tile;
 
 int dx[4] = { 0,1,0,-1 };
 int dy[4] = { 1,0,-1,0 };
 
 void bfs()
 {
-	deque<pair<int,int>> d;
-	d.push_back({ 1, 1 });
+	//우선순위 큐를 이용해 높이가 낮은 노드를 먼저 탐색하게 한다.
+	priority_queue<Node, vector<Node>> pq;
+	//처음 0,0노드에서 시작
+	Tile[0].setvisited();
+	pq.push(Tile[0]);
 
-	// 빈 방 먼저,
-	// 하,우 방향 먼저 부시기
-	while (!d.empty())
+	while (!pq.empty())
 	{
-		int fy = d.front().first;
-		int fx = d.front().second;
-
-		if (fy == M && fx == N)
+		Node n = pq.top();
+		//목적지에 다다른 노드의 높이를 출력한다.
+		if (n.ans(N-1,M-1))
 		{
-			cout << h[fy][fx];
-			break;
+			ans = min(ans, n.getheight());
 		}
+		pq.pop();
 
-		//빈 방을 모두 탐색하고,
-		//더 이상 탐색할 빈방이 없으면 
-		//벽을 하나씩 뚫어본다.
+		//상하좌우로 이동
 		for (int i = 0; i < 4; i++)
 		{
-			if (fy + dy[i] < 0 || fy + dy[i] >= M || fx + dx[i] < 0 || fx + dx[i] > N) continue;
-			if (visited[fy + dy[i]][fx + dx[i]]) continue;
-			visited[fy + dy[i]][fx + dx[i]] = 1;
-			if (!Tile[fy + dy[i]][fx + dx[i]]) d.push_front(make_pair(fy + dy[i], fx + dx[i]));
-			//벽을 뚫어야 하는 경우
+			int x = n.getxpos();
+			int y = n.getypos();
+			//x,y 죄표로 Tile에 저장된 값 인덱스 찾기
+			if (x + dx[i] < 0 || x + dx[i] >= N || y + dy[i] < 0 || y + dy[i] >= M) continue;
+
+			int curh = Tile[x  + y * M].getheight();
+			int nexth = Tile[x + dx[i] + (y + dy[i]) * M].getheight();
+			bool nextIswall = Tile[x + dx[i] + (y + dy[i]) * M].isWall();
+			bool nextIsvisited = Tile[x + dx[i] + (y + dy[i]) * M].isvisited();
+			bool next_Got_higher_height = (nextIswall && (nexth > curh + 1)) || (!nextIswall && (nexth > curh));
+
+			//이미 방문한 타일을 재방문한 경우
+			if (nextIsvisited && (!next_Got_higher_height))
+			{
+				//높이가 더 작은 경우가 존재한다면 갱신한다.
+				//그 외에는 재방문하지 않는다.
+				continue;
+			}
+
+			//방문하지 않은 타일을 방문했을 경우,
+			// 방문 표시를 업데이트 해준다.
+			if (!nextIsvisited)
+			{
+				Tile[x + dx[i] + (y + dy[i]) * M].setvisited();
+			}
+
+			//벽인 경우, 높이를 증가시킨다.
+			//증가시킨 높이가 정답보다 큰 경우, continue
+			if (nextIswall)
+			{
+				if (curh + 1 > ans) continue;
+				Tile[x + dx[i] + (y + dy[i]) * M].setheight(curh + 1);
+			}
+			//벽이 아니라면 높이는 유지된다.
 			else
 			{
-				//높이를 어떻게 지정해줘야할까..
-				h[fy + dy[i]][fx + dx[i]] = h[fy][fx] + 1;
-				Tile[fy + dy[i]][fx + dx[i]] = 0;
-				d.push_back(make_pair(fy + dy[i], fx + dx[i]));
+				Tile[x + dx[i] + (y + dy[i]) * M].setheight(curh);
 			}
+
+			//우선순위 큐에 탐색한 노드를 올린다. 
+			pq.push(Tile[x + dx[i] + (y + dy[i]) * M]);
 		}
+
 	}
+
 }
 
 int main()
@@ -78,19 +182,29 @@ int main()
 
 	cin >> N >> M;
 
+	//3 3
+	//011
+	//111
+	//110
+	// 0~8까지 Tile[8]까지 만들어진다.
+	// (2,0)에서 아래로 이동, (2,1)으로 이동하는 경우
+	// Tile[2] -> Tile[5]로 이동
+	// Tile[2 + 0 * 3] ->  Tile[2 + 1 * 3]
+
 	for (int i = 0; i < M; i++)
 	{
 		string s;
 		cin >> s;
 		for (int j = 0; j < N; j++)
 		{
-			Tile[i][j] = s[j] - '0';
+			Node node(i, j, s[j]);
+			Tile.push_back(node);
 		}
 	}
 
 	bfs();
+
+	cout << ans;
 	
 	return 0;
 }
-
-
